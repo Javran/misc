7ab81a7a15789cad1f61ab51532b403b868802da
Javran Cheng <Javran.c@gmail.com>
2014-08-04 09:45:40 +0800

    wheel guess

---
+++ wheelguess/wheelguess.hs
@@ -0,0 +1,31 @@
+import qualified System.IO.Strict as SIO
+import System.Environment
+import Control.Applicative
+import Data.Char
+import Text.Printf
+
+wordsFromDict :: FilePath -> IO [String]
+wordsFromDict p = lines <$> SIO.readFile p
+
+pruneDict :: Int -> [String] -> [String]
+pruneDict l = (map . map) toLower . filter allLetter . filter ((== l) . length)
+    where
+        allLetter = all (\x -> x `elem` ['a'..'z']
+                            || x `elem` ['A'..'Z'])
+
+guessWords :: [String] -> [String] -> [String]
+guessWords dict wheels = filter isWord $ sequence wheels'
+    where
+        wheels' = (map . map) toLower wheels
+        dict' = pruneDict (length wheels) dict
+        isWord = (`elem` dict')
+
+main :: IO ()
+main = do
+    (dpath:_) <- getArgs
+    d <- wordsFromDict dpath
+    let guesses = guessWords d wheels
+    mapM_ putStrLn guesses
+    printf "%d valid words found.\n" (length guesses)
+    where
+        wheels = words "swbpfmdtal porilcetna erilanutos ldaosknrte lsnthyder"
