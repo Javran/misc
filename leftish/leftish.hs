391286992bb695ebc617b378751d98837ab0b95f
Javran Cheng <Javran.c@gmail.com>
2014-08-05 18:50:44 +0800

    leftish - optimized

--- leftish/leftish.hs
+++ leftish/leftish.hs
@@ -1,14 +1,35 @@
-import Control.Monad
-import Control.Monad.Random
+module Leftish
+    ( BTree
+    , merge
+    , empty
+    , singleton
+    , insert
+    , deleteMin
+    , toList
+    , toAscList
+    , fromList
+    )
+where
+
+import qualified Data.Sequence as Seq
+import Data.Sequence (Seq, (|>), ViewL(..), viewl)
+import Data.Function
+
+-- import Control.Monad
+-- import Control.Monad.Random hiding (fromList)
 
 data BTree a
     = Nil
-    | Node Int !a (BTree a) (BTree a)
+    | Node !Int !a !(BTree a) !(BTree a)
       deriving (Show)
 
 dist :: BTree a -> Int
 dist Nil = -1
 dist (Node d _ _ _) = d
+
+value :: BTree a -> a
+value Nil = error "value on Nil"
+value (Node _ v _ _) = v
 
 empty :: BTree a
 empty = Nil
@@ -19,13 +40,13 @@ singleton x = Node 0 x Nil Nil
 merge :: Ord a => BTree a -> BTree a -> BTree a
 merge Nil r = r
 merge l Nil = l
-merge ta@(Node _ a _ _) tb@(Node _ b _ _) =
-    let (treeL,treeR) = if a <= b then (ta,tb) else (tb,ta)
+merge ta tb = Node newDist v1 newL newR
+    where
+        (treeL,treeR) = if value ta <= value tb then (ta,tb) else (tb,ta)
         (Node _ v1 l1 r1) = treeL
         r1' = merge r1 treeR
         (newL,newR) = if dist r1' > dist l1 then (r1',l1) else (l1,r1')
         newDist = 1 + dist newR
-    in Node newDist v1 newL newR
 
 insert :: Ord a => a -> BTree a -> BTree a
 insert v = merge (singleton v)
@@ -40,9 +61,23 @@ toAscList t =
     let (v,t') = deleteMin t
     in v:toAscList t'
 
-main :: IO ()
-main = do
-    xs <- liftM (take 500000) $ getRandomRs (0 :: Int,1000000)
-    let lt = foldr insert empty xs
-        result = toAscList lt
-    print $ and $ zipWith (<=) result (tail result)
+toList :: BTree a -> [a]
+toList Nil = []
+toList (Node _ v l r) = v : ((++) `on` toList) l r
+
+fromList :: Ord a => [a] -> BTree a
+fromList = fromSeq . fmap singleton . Seq.fromList
+
+fromSeq :: Ord a => Seq (BTree a) -> BTree a
+fromSeq s = case viewl s of
+    EmptyL -> Nil
+    v1 :< seq1 -> case viewl seq1 of
+        EmptyL -> v1
+        v2 :< seq2 -> fromSeq (seq2 |> merge v1 v2)
+
+-- main :: IO ()
+-- main = do
+--      xs <- liftM (take 100000) $ getRandomRs (0 :: Int,1000000)
+--      let lt = fromList xs
+--          result = toAscList lt
+--      print $ and $ zipWith (<=) result (tail result)
