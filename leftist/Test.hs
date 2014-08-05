472a8125cbd30c3a363ab8c64da0c75a3b25553f
Javran Cheng <Javran.c@gmail.com>
2014-08-05 20:02:20 +0800

    comments

--- leftist/Test.hs
+++ leftist/Test.hs
@@ -6,13 +6,16 @@ import qualified Leftist as L
 
 import Test.QuickCheck
 
+-- | a list is ascending iff. xs[i] <= xs[i+1] holds for all valid i
 isAscending :: Ord a => [a] -> Bool
 -- zipWith _ [] (tail []) is safe
 isAscending xs = and (zipWith (<=) xs (tail xs))
 
+-- | a sorted list is an ascending list
 prop_Sort :: [Int] -> Bool
 prop_Sort = isAscending . L.sort
 
+-- | test if the leftist tree property holds
 prop_LeftishProp :: [Int] -> Bool
 prop_LeftishProp = L.propertyHolds . L.fromList
 
