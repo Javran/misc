d401718b632b79cefaf55353e90497a8ae5a3ae5
Javran Cheng <Javran.c@gmail.com>
2014-08-05 19:39:28 +0800

    quickcheck tests

---
+++ leftish/Test.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Main
+where
+
+import qualified Leftish as L
+
+import Test.QuickCheck
+
+isAscending :: Ord a => [a] -> Bool
+-- zipWith _ [] (tail []) is safe
+isAscending xs = and (zipWith (<=) xs (tail xs))
+
+prop_Sort :: [Int] -> Bool
+prop_Sort = isAscending . L.sort
+
+prop_LeftishProp :: [Int] -> Bool
+prop_LeftishProp = L.propertyHolds . L.fromList
+
+return []
+
+main :: IO Bool
+main = $quickCheckAll
