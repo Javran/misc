57ef28a2b5ad7b5e295e7ff3b3ae6b81cf47824f
Javran Cheng <Javran.c@gmail.com>
2014-10-08 23:43:50 -0400

    syb - different strategies

--- syb-play/src/Main.hs
+++ syb-play/src/Main.hs
@@ -1,18 +1,41 @@
 module Main where
 
+import Control.Applicative
 import Data.Tree
+import Data.Monoid
 import Data.Generics
 
+-- | a tree containing integers
 exTree1 :: Tree Int
-exTree1 = Node 1 [Node 2 [], Node 3 [], Node 4 [Node 5 [], Node 6 [Node 7 []]]]
+exTree1 = Node (1 :: Int) [Node 2 [], Node 3 [], Node 4 [Node 5 [], Node 6 [Node 7 []]]]
+
+-- | exTree1 converted to a tree of strings
+exTree2 :: Tree String
+exTree2 = show <$> exTree1
 
 printTree :: Show a => Tree a -> IO ()
 printTree t = putStrLn (drawTree (fmap show t))
 
-accumDirectChildren :: Tree Int -> Tree Int
-accumDirectChildren (Node a s) = Node (a + sum (map rootLabel s)) s
+foldWithDirectChildren :: (a -> a -> a) -> a -> Tree a -> Tree a
+foldWithDirectChildren f z (Node a s) = Node (a `f` foldl f z (map rootLabel s)) s
 
 main :: IO ()
-main = printTree exTree1
-    >> printTree (everywhere (mkT accumDirectChildren) exTree1)
-    >> printTree (everywhere' (mkT accumDirectChildren) exTree1)
+main = do
+    printTree exTree1
+
+    -- drawback: cannot deduce a type
+    printTree (everywhere  (mkT (foldWithDirectChildren (+) 0 :: Tree Int -> Tree Int)) exTree1)
+    printTree (everywhere' (mkT (foldWithDirectChildren (+) 0 :: Tree Int -> Tree Int)) exTree1)
+    -- if type mismatches, nothing will happen
+    -- (this might potentially be bad)
+
+    printTree (everywhere' (mkT (foldWithDirectChildren (+) 0 :: Tree Int -> Tree Int)) exTree2)
+
+    let plusStr a b
+          | null a = b
+          | null b = a
+          | otherwise = a ++ "|" ++ b
+
+    -- the different strategies
+    printTree (everywhere  (mkT (foldWithDirectChildren plusStr "")) exTree2)
+    printTree (everywhere' (mkT (foldWithDirectChildren plusStr "")) exTree2)
