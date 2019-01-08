6711def095e36abe1c1014d5446ced82a56e7a6f
Javran Cheng <Javran.c@gmail.com>
2019-01-07 22:27:09 -0800

    necessary files

---
+++ naka-no-more/NoMore.hs
@@ -0,0 +1,17 @@
+{-
+  Script for wiping out something entirely
+
+  - files.txt is prepare by calling window.getShipImgPath on poi-plugin-navy-album
+    exhausting all valid parameters.
+
+-}
+
+module NoMore
+  ( main
+  ) where
+
+import Turtle
+import Prelude hiding (FilePath)
+
+main :: IO ()
+main = pure ()
