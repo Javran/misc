bd5008910c281d8bca31e257b6a57e386b21bfe3
Javran Cheng <Javran.c@gmail.com>
2019-02-23 00:03:09 -0800

    notes

--- reflection/reflection.hs
+++ reflection/reflection.hs
@@ -13,13 +13,23 @@ func x = [x+1, x+2, x*10]
 tyFunc :: forall r. (forall s . Reifies s F => Proxy s -> r) -> r
 tyFunc = reify func
 
+
+tyFunc' :: Reifies s F => Proxy s
+tyFunc' =
+    -- `reify func id` won't work
+    reify func $ \Proxy -> Proxy
+
 exec :: Reifies s F => Proxy s -> IO ()
 exec p = print (reflect p 20)
 
 main :: IO ()
 main = do
   print (tyFunc $ ($ 20) . reflect)
-  -- `reify func id` won't work,
+
   -- but it seems as long as we use `reify` to "connect"
   -- consumer (exec) with producer (func), it works fine
   reify func exec
+
+  -- following line doesn't work, GHC has trouble to unify two s variable,
+  -- perhaps that's exactly the point
+  -- exec tyFunc'
