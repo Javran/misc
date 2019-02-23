4fe30fb7e18f5f61a308650339cfd6b228287626
Javran Cheng <Javran.c@gmail.com>
2019-02-22 23:52:10 -0800

    more example

--- reflection/reflection.hs
+++ reflection/reflection.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleContexts, RankNTypes, ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleContexts, RankNTypes #-}
 module Main where
 
 import Data.Reflection
@@ -13,5 +13,13 @@ func x = [x+1, x+2, x*10]
 tyFunc :: forall r. (forall s . Reifies s F => Proxy s -> r) -> r
 tyFunc = reify func
 
+exec :: Reifies s F => Proxy s -> IO ()
+exec p = print (reflect p 20)
+
 main :: IO ()
-main = print (tyFunc $ ($ 20) . reflect)
+main = do
+  print (tyFunc $ ($ 20) . reflect)
+  -- `reify func id` won't work,
+  -- but it seems as long as we use `reify` to "connect"
+  -- consumer (exec) with producer (func), it works fine
+  reify func exec
