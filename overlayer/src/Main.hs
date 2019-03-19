da588adb59d1d2f0df3ce3369bfbdf18bafcc8bb
Javran Cheng <Javran.c@gmail.com>
2019-03-18 23:14:23 -0700

    finish processing

--- overlayer/src/Main.hs
+++ overlayer/src/Main.hs
@@ -5,34 +5,43 @@
   #-}
 module Main (main) where
 
-import Turtle hiding (printf,f,g)
-import Prelude hiding (FilePath)
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Lazy as BSL
+import Control.Exception
 import Control.Monad
-import Text.Printf
+import Data.Aeson
+import qualified Data.ByteString.Lazy as BSL
+import Data.HashMap.Strict as HM
+import Data.List
+import qualified Data.Text as T
+import qualified Graphics.Image as HIP
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS (tlsManagerSettings)
-import Data.Aeson
-import Data.HashMap.Strict as HM
-import qualified Data.Text as T
-import qualified Graphics.Image.IO.Formats as HIP
-import qualified Graphics.Image as HIP
+import Prelude hiding (FilePath)
+import Text.Printf
+import Turtle hiding (printf,f,g,sort,e)
 
 getPoiCachePath :: MonadIO m => m FilePath
 getPoiCachePath =
   (\p -> p </> ".config" </> "poi" </> "MyCache" </> "KanColle") <$> home
+
+storeProcessedMap :: MonadIO m => Int -> Int -> Img -> m ()
+storeProcessedMap area num img = do
+  cachePath <- getPoiCachePath
+  let imgDirPath =
+        cachePath </> decodeString (printf "kcs2/resources/map/%03d" area)
+      imgPath = imgDirPath </> decodeString (printf "%02d_image.png" num)
+  mktree imgDirPath
+  liftIO $ HIP.writeImageExact HIP.PNG [] (encodeString imgPath) img
 
 serverBase :: String
 serverBase = "http://203.104.209.134"
 
 coords :: [(Int,Int)]
 coords =
-  [ (601, 261)
-  , (796, 261)
-  , (994, 261)
-  , (703, 499)
-  , (899, 499)
+  [ (261, 601)
+  , (261, 796)
+  , (261, 994)
+  , (499, 703)
+  , (499, 899)
   ]
 
 getMapUrls :: Int -> Int -> (String, String)
@@ -50,17 +59,16 @@ extractFrameInfo area num raw
   , Just (Object frameDetail) <- HM.lookup "frame" mapInfoRaw
   = let g k | Just (Number v) <- HM.lookup k frameDetail = round v
         g _ = error "unreachable"
-    in ((g "x", g "y"), (g "w", g "h"))
+    in -- note the difference:
+       -- in HIP x is the vertical axis pointing down from top left corner
+       ((g "y", g "x"), (g "w", g "h"))
   | otherwise = error "extraction error"
   where
     mapInfoKey = T.pack $ printf "map%03d%02d_map%d-%d" area num area num
 
-main :: IO ()
-main = do
-  mgr <- newManager tlsManagerSettings
-  let area = 5
-      num = 2
-      (imgUrl, infoUrl) = getMapUrls area num
+processGameMap :: Manager -> Int -> Int -> IO ()
+processGameMap mgr area num = do
+  let (imgUrl, infoUrl) = getMapUrls area num
       simpleReq url = do
         req <- parseUrlThrow url
         responseBody <$> httpLbs req mgr
@@ -71,15 +79,27 @@ main = do
         HIP.decode HIP.PNG (BSL.toStrict imgData)
   let frameData =
         HIP.makeImageR HIP.VS (35,141) (const $ HIP.PixelRGBA 0xff 0x00 0xdb 0xff) :: Img
-  let ((offX, offY),(1200,720)) =  extractFrameInfo area num info
+  let ((offX,offY),(1200,720)) = extractFrameInfo area num info
       imposed = foldl imp mapImgData coords
         where
-          imp img (cy,cx) =
-             HIP.superimpose (x'+2,y'+2) cropped $
-              HIP.superimpose xy' frameData img
+          imp img (cx,cy) =
+            HIP.superimpose (x'+2,y'+2) cropped $
+              HIP.superimpose (x',y') frameData img
             where
-              xy'@(x',y') = (offX+cx, offY+cy)
+              (x',y') = (offX+cx, offY+cy)
               cropped = HIP.crop (x'+2,y'+2) (35-4,141-4) mapImgData
 
-  print $ extractFrameInfo area num info
-  HIP.writeImageExact HIP.PNG [] "/tmp/test.png" imposed
+  storeProcessedMap area num imposed
+
+allGameMaps :: [(Int, Int)]
+allGameMaps =
+  sort $ [(area,num) | area <- [1..6], num <- [1..5]] <> [(1,6),(7,1),(7,2)]
+
+main :: IO ()
+main = do
+  mgr <- newManager tlsManagerSettings
+  forM_ allGameMaps $ \(area,num) -> do
+    printf "Processing %d-%d\n" area num
+    catch (processGameMap mgr area num) $ \(e :: SomeException) -> do
+      putStr "Exception caught: "
+      putStrLn (displayException e)
