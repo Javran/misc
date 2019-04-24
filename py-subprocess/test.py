18f0f07a5cacd82e930c022d877bf30de70181af
Javran Cheng <Javran.c@gmail.com>
2019-04-24 00:07:10 -0700

    simplify with pipe
    turns out it's actually possible to relay subprocess's
    output as line buffering.

--- py-subprocess/test.py
+++ py-subprocess/test.py
@@ -6,16 +6,19 @@ import tempfile
 
 
 def main():
-    with tempfile.NamedTemporaryFile(mode='w', buffering=1) as f_out, \
-         tempfile.NamedTemporaryFile(mode='w', buffering=1) as f_err:
-        proc = subprocess.Popen(
-            ['./out.sh'],
-            shell=True,
-            stdin=subprocess.PIPE,
-            stdout=f_out,
-            stderr=f_err,
-        )
-        proc.wait()
+    proc = subprocess.Popen(
+        ['./out.sh'],
+        shell=True,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    for line in proc.stdout:
+        print(f'subproc stdout:{line}')
+    for line in proc.stderr:
+        print(f'subproc stderr:{line}')
+    proc.wait()
+
 
 if __name__ == '__main__':
     main()
