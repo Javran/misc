0ad3a5f92e9c159cd064a23dd23b9da187908575
Javran Cheng <Javran.c@gmail.com>
2019-05-02 21:54:01 -0700

    fix build - turns out we need 0.5+ for this.

--- proto-playground/src/Main.hs
+++ proto-playground/src/Main.hs
@@ -1,25 +1,73 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE
+    OverloadedStrings
+  , OverloadedLabels
+  , GeneralizedNewtypeDeriving
+  , TypeApplications
+  #-}
 module Main
   ( main
   ) where
 
 import Proto.Person as P
-import Proto.Person_Fields as P
 import Data.ProtoLens (defMessage, showMessage)
+import qualified Proto.Coffee.Order as P
+import Data.ProtoLens.Labels ()
+
 import Lens.Micro
+import Lens.Micro.Extras (view)
+
+-- | Smart constructor for making an 'Americano'
+--   with a price of €2.70
+americano :: P.Coffee
+americano =
+  defMessage
+      & #cost      .~ 2.70
+      & #americano .~ defMessage
+
+-- | Smart constructor for making an 'Latte'
+--   with a price of €3.20
+latte :: P.Coffee
+latte =
+  defMessage
+      & #cost  .~ 3.20
+      & #latte .~ defMessage
+
+-- | Smart constructor for making an 'FlatWhite'
+--   with a price of €3.30
+flatWhite :: P.Coffee
+flatWhite =
+  defMessage
+      & #cost      .~ 3.30
+      & #flatWhite .~ defMessage
+
+-- | Smart constructor for making an 'Americano'
+--   with a price of €3.00
+cappuccino :: P.Coffee
+cappuccino =
+  defMessage
+      & #cost       .~ 3.00
+      & #cappuccino .~ defMessage
+
+-- | Smart constructor for making an 'Americano'
+--   with a price of €3.50
+mocha :: P.Coffee
+mocha =
+  defMessage
+      & #cost  .~ 3.50
+      & #mocha .~ defMessage
 
 person :: P.Person
 person =
     defMessage
-    & P.name .~ "Fintan"
-    & P.age .~ 24
-    & P.addresses .~ [address]
+    & #name .~ "Fintan"
+    & #age .~ 24
+    & #addresses .~ [address]
   where
     address :: P.Address
     address =
         defMessage
-        & P.street .~ "Yolo street"
-        & P.zipCode .~ "D8"
+        & #street .~ "Yolo street"
+        & #zipCode .~ "D8"
 
 main :: IO ()
-main = putStrLn . showMessage $ person
+main = pure ()
