335276edd7474a6e6fe81f5a097fda7441ad79ea
Javran Cheng <Javran.c@gmail.com>
2019-05-17 23:37:32 -0700

    working!

--- haskell-ffi/src/Main.hs
+++ haskell-ffi/src/Main.hs
@@ -1,6 +1,27 @@
+{-# LANGUAGE ForeignFunctionInterface, ScopedTypeVariables #-}
 module Main
   ( main
   ) where
 
+import Foreign.Ptr
+import Foreign.Marshal.Alloc
+
+data NeumaierObj
+
+foreign import ccall "neumaier_sum_init"
+  neumaierSumInit :: Ptr NeumaierObj -> IO ()
+
+foreign import ccall "neumaier_sum_add"
+  neumaierSumAdd :: Ptr NeumaierObj -> Double -> IO ()
+
+foreign import ccall "neumaier_sum_done"
+  neumaierSumDone :: Ptr NeumaierObj -> IO Double
+
 main :: IO ()
-main = pure ()
+main = do
+  let xs = [1,1.1..20]
+  result <- allocaBytes 16 $ \(p :: Ptr NeumaierObj) -> do
+    neumaierSumInit p
+    mapM_ (neumaierSumAdd p) xs
+    neumaierSumDone p
+  print (result :: Double, sum xs)
