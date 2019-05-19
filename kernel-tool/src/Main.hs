ad44682f1ea806cc3fa1d1aa0ac8873612799b8e
Javran Cheng <Javran.c@gmail.com>
2019-05-18 23:13:07 -0700

    proof of concept
    now the workaround does work, a bit improvement
    is made by invoking $SHELL rather than having it fixed to bash

--- kernel-tool/src/Main.hs
+++ kernel-tool/src/Main.hs
@@ -1,6 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main
   ( main
   ) where
 
+import System.Process
+import System.Environment
+import Turtle
+
 main :: IO ()
-main = pure ()
+main = do
+    cd "/"
+    shBin <- getEnv "SHELL"
+    setEnv "KERNEL_TOOL" "Test"
+    let p = (System.Process.proc shBin [])
+            { std_in  = Inherit
+            , std_out = Inherit
+            , std_err = Inherit
+            , delegate_ctlc = True
+            }
+    (_, _, _, ph) <- createProcess p
+    _ <- waitForProcess ph
+    pure ()
