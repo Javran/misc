abedb9ab66fc44b1afd1cbb5eaee79bfef7177ae
Javran Cheng <Javran.c@gmail.com>
2019-05-19 22:13:40 -0700

    copy over config if missing

--- kernel-tool/src/CommandSwitch.hs
+++ kernel-tool/src/CommandSwitch.hs
@@ -7,8 +7,10 @@ import Turtle hiding (w)
 import Text.ParserCombinators.ReadP as P
 import Data.Char
 import Data.Maybe
+import System.Directory
 
 import qualified Data.Text as T
+import qualified Data.Text.IO as T
 
 {-
   kernel info: (<item number>, (<kernel version>, <is selected>))
@@ -27,15 +29,26 @@ kernelInfoP = do
 
 cmdSwitch :: IO ()
 cmdSwitch = do
+  -- get info about existing kernel versions
   (ExitSuccess, out) <- procStrict "eselect" ["kernel", "list"] ""
   let _:rawKVers = T.lines out
       kVers = fromJust . parseKernelInfo <$> rawKVers
       latestKVer = last kVers
-  unless (snd . snd $ latestKVer) $ do
-    putStrLn "Switching to latest kernel version ..."
-    (ExitSuccess, _) <-
+  -- switch to latest (last) kernel version if we haven't done so.
+  if snd . snd $ latestKVer
+    then putStrLn "Skipping kernel version switch ..."
+    else do
+      putStrLn "Switching to latest kernel version ..."
+      (ExitSuccess, _) <-
         procStrict
           "eselect"
           ["kernel", "set", T.pack . show . fst $ latestKVer]
           ""
+      pure ()
+  -- copy over .config from current kernel if the file is missing
+  cfgFileExist <- doesFileExist "/usr/src/linux/.config"
+  unless cfgFileExist $ do
+    putStrLn "Creating .config from current running config."
+    (ExitSuccess, cfgContent) <- procStrict "zcat" ["/proc/config.gz"] ""
+    T.writeFile "/usr/src/linux/.config" cfgContent
     pure ()
