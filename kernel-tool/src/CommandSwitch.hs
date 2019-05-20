4f48904890fd06ef2cb3435800150970c97144f2
Javran Cheng <Javran.c@gmail.com>
2019-05-19 21:55:20 -0700

    done version switch

--- kernel-tool/src/CommandSwitch.hs
+++ kernel-tool/src/CommandSwitch.hs
@@ -4,10 +4,15 @@ module CommandSwitch
   ) where
 
 import Turtle hiding (w)
-import qualified Data.Text as T
 import Text.ParserCombinators.ReadP as P
 import Data.Char
+import Data.Maybe
 
+import qualified Data.Text as T
+
+{-
+  kernel info: (<item number>, (<kernel version>, <is selected>))
+ -}
 parseKernelInfo :: T.Text -> Maybe (Int, (String, Bool))
 parseKernelInfo t = case readP_to_S (skipSpaces *> kernelInfoP <* P.eof) (T.unpack t) of
   [(r,"")] -> Just r
@@ -24,4 +29,13 @@ cmdSwitch :: IO ()
 cmdSwitch = do
   (ExitSuccess, out) <- procStrict "eselect" ["kernel", "list"] ""
   let _:rawKVers = T.lines out
-  mapM_ (print . parseKernelInfo) rawKVers
+      kVers = fromJust . parseKernelInfo <$> rawKVers
+      latestKVer = last kVers
+  unless (snd . snd $ latestKVer) $ do
+    putStrLn "Switching to latest kernel version ..."
+    (ExitSuccess, _) <-
+        procStrict
+          "eselect"
+          ["kernel", "set", T.pack . show . fst $ latestKVer]
+          ""
+    pure ()
