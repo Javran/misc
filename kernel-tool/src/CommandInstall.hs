cf56ac41542f70a19b00fe52d8b4fcb52f3a3607
Javran Cheng <Javran.c@gmail.com>
2019-05-19 22:50:15 -0700

    plan for grub-conf update

--- kernel-tool/src/CommandInstall.hs
+++ kernel-tool/src/CommandInstall.hs
@@ -6,10 +6,25 @@ module CommandInstall
 import Turtle
 
 cmdInstall :: IO ()
-cmdInstall = sh $ do
-  pushd "/usr/src/linux"
-  (ExitSuccess, _) <- procStrict "make" ["install"] ""
-  (ExitSuccess, _) <- procStrict "make" ["modules_install"] ""
-  (ExitSuccess, _) <- procStrict "emerge" ["@module-rebuild"] ""
-  -- TODO: update grub.conf
-  pure ()
+cmdInstall = do
+  sh $ do
+    pushd "/usr/src/linux"
+    (ExitSuccess, _) <- procStrict "make" ["install"] ""
+    (ExitSuccess, _) <- procStrict "make" ["modules_install"] ""
+    (ExitSuccess, _) <- procStrict "emerge" ["@module-rebuild"] ""
+    pure ()
+  updateGrubConf
+
+{-
+  TODO: recognize existing kernels and update grub.conf using a template.
+
+  - to recognize a valid kernel version:
+
+    + config-XXX, System.map-XXX, vmlinux-XXX must all exist, with XXX being exactly the same
+  - use http://hackage.haskell.org/package/natural-sort or something alike to sort
+    kernel versions
+  - use http://hackage.haskell.org/package/microstache for generating grub.conf
+
+ -}
+updateGrubConf :: IO ()
+updateGrubConf = pure ()
