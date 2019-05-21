7111511efaf79307664a02b5452c9f026105feee
Javran Cheng <Javran.c@gmail.com>
2019-05-20 22:55:43 -0700

    identify possible kernel sets

--- kernel-tool/src/CommandInstall.hs
+++ kernel-tool/src/CommandInstall.hs
@@ -3,7 +3,10 @@ module CommandInstall
   ( cmdInstall
   ) where
 
-import Turtle
+import Data.List
+import qualified Control.Foldl as Foldl
+import qualified Data.Text as T
+import Turtle hiding (fp)
 
 cmdInstall :: IO ()
 cmdInstall = do
@@ -27,4 +30,18 @@ cmdInstall = do
 
  -}
 updateGrubConf :: IO ()
-updateGrubConf = pure ()
+updateGrubConf = do
+  -- get potential options for vmlinuz-* files.
+  kernelSets <- reduce Foldl.list $
+    ls "/boot/" >>= \fp -> do
+      let fName = encodeString . filename $ fp
+          vmlzMagic = "vmlinuz-"
+      True <- testfile fp
+      guard $ vmlzMagic `isPrefixOf` fName
+      -- we say "X.Y.Z-gentoo" is a KernelSet,
+      -- if all of vmlinuz, System.map and config exists
+      let kernelSet = T.pack $ drop (length vmlzMagic) fName
+      True <- testfile ("/boot" </> fromText ("System.map-" <> kernelSet))
+      True <- testfile ("/boot" </> fromText ("config-" <> kernelSet))
+      pure kernelSet
+  print kernelSets
