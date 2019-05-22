ba27a3e96033548c80d470105fd662766f9c3a89
Javran Cheng <Javran.c@gmail.com>
2019-05-21 22:33:40 -0700

    allow few steps to fail

--- kernel-tool/src/CommandInstall.hs
+++ kernel-tool/src/CommandInstall.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, TypeApplications, LambdaCase #-}
 module CommandInstall
   ( cmdInstall
   ) where
@@ -9,24 +9,29 @@ import System.Environment
 import qualified Control.Foldl as Foldl
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as TL
-import Turtle hiding (fp)
+import Turtle hiding (fp, e)
 import Algorithms.NaturalSort
 import Text.Microstache
 import qualified Data.Aeson as Aeson
 import qualified Data.HashMap.Lazy as HM
 import qualified Data.Vector as Vec
 import qualified Data.Text.IO as T
+import Control.Exception
 
 cmdInstall :: IO ()
 cmdInstall =  sh $ do
   pushd "/usr/src/linux"
-  -- TODO: tolerate failure.
   (ExitSuccess, _) <- procStrict "make" ["install"] ""
   (ExitSuccess, _) <- procStrict "make" ["modules_install"] ""
-  liftIO updateGrubConf
-  (ExitSuccess, _) <- procStrict "emerge" ["@module-rebuild"] ""
-  pure ()
-
+  liftIO $ Control.Exception.try @SomeException updateGrubConf >>= \case
+    Left e -> putStrLn $ "Allowed to fail: " <> displayException e
+    Right _ -> pure ()
+  r <- procStrict "emerge" ["@module-rebuild"] ""
+  case r of
+    (ExitSuccess, _) -> pure ()
+    (ExitFailure ec, out) -> liftIO $ do
+      putStrLn $ "Exitcode=" <> show ec <> " (allowed to fail)"
+      T.putStrLn out
 
 {-
   Recognize existing kernels and update grub.conf using a template.
