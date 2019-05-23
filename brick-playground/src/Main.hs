02778f0a60736740be010683cbf600e42ab9ad7f
Javran Cheng <Javran.c@gmail.com>
2019-05-22 23:26:31 -0700

    proper center

--- brick-playground/src/Main.hs
+++ brick-playground/src/Main.hs
@@ -6,13 +6,14 @@ import Brick
 import Brick.Widgets.Center
 import Brick.Widgets.Border
 import Brick.Widgets.Border.Style
+import Data.List
 
 ui :: Widget ()
-ui =
-  withBorderStyle unicode $
-    center $ vBox (replicate 6 wLine)
+ui = center $ vBox (replicate 4 wLine)
   where
-    wLine = hBox $ fmap (border . str . pure) ['A'..'Z']
+    wLine =
+      hBox $ intersperse (str "|")
+        $ vLimit 5 . hLimit 10 . str <$> words "AAAA BBBB CCCC DDDD"
 
 main :: IO ()
 main = simpleMain ui
