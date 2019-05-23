9b7f1c3d1d2eb60b597d29d7b5774241167fbfaa
Javran Cheng <Javran.c@gmail.com>
2019-05-22 22:57:19 -0700

    grid with gap

--- brick-playground/src/Main.hs
+++ brick-playground/src/Main.hs
@@ -3,9 +3,16 @@ module Main
   ) where
 
 import Brick
+import Brick.Widgets.Center
+import Brick.Widgets.Border
+import Brick.Widgets.Border.Style
 
 ui :: Widget ()
-ui = str "Saluton!"
+ui =
+  withBorderStyle unicode $
+    center $ vBox (replicate 6 wLine)
+  where
+    wLine = hBox $ fmap (border . str . pure) ['A'..'Z']
 
 main :: IO ()
 main = simpleMain ui
