341a364efc18efdfae7c2b9cc026e99e500c30f5
Javran Cheng <Javran.c@gmail.com>
2019-05-26 00:00:15 -0700

    clean command envs

--- kernel-tool/src/CommandClean.hs
+++ kernel-tool/src/CommandClean.hs
@@ -3,9 +3,21 @@ module CommandClean
   ) where
 
 {-
+  environment variables:
+
+  - KERNEL_TOOL_CLEAN_LIMIT: numbers of kernels to keep
+    default=2
+  - KERNEL_TOOL_CLEAN_BACKUP_DIR: backup directory.
+    default=/boot/backup
+ -}
+
+{-
+  TODO:
   for now, `clean` command only aims at:
-  - moving stuff from /boot/backup/ into /tmp
-  - moving old kernel into /boot/backup/
+  - moving stuff from backup dir into /tmp/<random dir>
+  - moving old kernel into backup dir
+  - moving partial kernel files into backup
+    (those that have vmlinuz or System.map or config but not all of 3)
  -}
 cmdClean :: IO ()
 cmdClean = pure ()
