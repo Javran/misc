f2b1ed7cbcc2a06ba3905dd848de99076b18eea3
Javran Cheng <Javran.c@gmail.com>
2019-09-09 22:32:58 -0700

    for setting file perm.

--- log-playground/src/Main.hs
+++ log-playground/src/Main.hs
@@ -6,26 +6,37 @@ module Main
 {-
   Experiment some log-to-file facility
   together with UNIX file permission setup and stuff.
-
-  TODO: mechanism to make sure that the file has rw- --- --- ?
  -}
 
 import Prelude hiding (log)
 import Control.Logging
+import Control.Monad
+import System.Posix.Files
 
 main :: IO ()
-main = loggingContext $ do
-  -- see: Data.Time.Format
-  setLogTimeFormat "%F %T"
-  log "stuff"
-  log "aaa"
-  log "bbb"
-  logS "uvuv" "fff" -- xxxS family provides an additional source to print out.
-  warnS "component" "warning message."
-  pure ()
+main = do
+    {-
+      To make sure that the file has the right permission bit,
+      a quick and simple solution might be just to set those bits prior
+      to any logging events.
+      But admittedly this isn't really a good solution, perhaps don't bother
+      with making this right at this moment.
+     -}
+    when fileLogging $
+      setFileMode logFilePath 0o600
+    loggingContext $ do
+      {-
+        see: Data.Time.Format
+       -}
+      setLogTimeFormat "%F %T"
+      log "stuff"
+      logS "uvuv" "fff" -- xxxS family provides an additional source to print out.
+      warnS "component" "warning message."
+      pure ()
   where
-    fileLogging = False
+    logFilePath = "/tmp/log-playground.log"
+    fileLogging = True
     loggingContext =
       if fileLogging
-        then withFileLogging  "/tmp/log-playground.log"
+        then withFileLogging logFilePath
         else withStdoutLogging
