2d9cb8bc20c0f4cf045bce752663b906294effc1
Javran Cheng <Javran.c@gmail.com>
2019-09-09 22:16:19 -0700

    some notes on logging.

--- log-playground/src/Main.hs
+++ log-playground/src/Main.hs
@@ -3,19 +3,29 @@ module Main
   ( main
   ) where
 
-
 {-
   Experiment some log-to-file facility
   together with UNIX file permission setup and stuff.
+
+  TODO: mechanism to make sure that the file has rw- --- --- ?
  -}
+
 import Prelude hiding (log)
 import Control.Logging
 
 main :: IO ()
-main = withFileLogging "/tmp/log-playground.log" $ do
+main = loggingContext $ do
+  -- see: Data.Time.Format
+  setLogTimeFormat "%F %T"
   log "stuff"
   log "aaa"
   log "bbb"
-  logS "uvuv" "fff"
+  logS "uvuv" "fff" -- xxxS family provides an additional source to print out.
   warnS "component" "warning message."
   pure ()
+  where
+    fileLogging = False
+    loggingContext =
+      if fileLogging
+        then withFileLogging  "/tmp/log-playground.log"
+        else withStdoutLogging
