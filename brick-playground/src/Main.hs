9ed5c15a4d540d3c0f32a184242f79f4eb76a734
Javran Cheng <Javran.c@gmail.com>
2019-09-12 23:14:35 -0700

    some interesting centering...

--- brick-playground/src/Main.hs
+++ brick-playground/src/Main.hs
@@ -7,10 +7,10 @@ import Brick.Widgets.Center
 import Brick.Widgets.Border
 
 ui :: Widget ()
-ui = center $ joinBorders $ border $ vLimit 10 $ hLimit 20 $ row <+> vBorder <+> row <+> vBorder <+> row
+ui = center $ joinBorders $ border $ setAvailableSize (20,10) $ row <+> vBorder <+> center row <+> vBorder <+> row
   where
-    row = wTest <=> hBorder <=> wTest <=> hBorder <=> wTest
-    wTest = vLimit 2 . hLimit 4 $ str "text"
+    row = wTest <=> hBorder <=> center wTest <=> hBorder <=> wTest
+    wTest = setAvailableSize (5,3) $ str "text"
 
 main :: IO ()
 main = simpleMain ui
