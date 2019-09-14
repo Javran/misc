aebcbff759dad8ebde1ff50b8fe68035ff1437c1
Javran Cheng <Javran.c@gmail.com>
2019-09-13 23:07:33 -0700

    cleanup the mess a bit.

--- brick-playground/src/Main.hs
+++ brick-playground/src/Main.hs
@@ -7,12 +7,16 @@ import Brick.Widgets.Center
 import Brick.Widgets.Border
 import Data.List
 
+vhLimit :: Int -> Int -> Widget a -> Widget a
+vhLimit v h = vLimit v . hLimit h
+
 ui :: Widget ()
-ui = joinBorders $ center $ border $ vLimit 15 $ hLimit 15 grid
+ui = joinBorders $ center $ border $ vhLimit (v*8+7) (v*8+7) grid
   where
+    (v,h) = (2,2)
     grid = center $ vBox (intersperse hBorder (replicate 8 row))
-    row = center $ hBox (intersperse vBorder (replicate 8 wTest))
-    wTest = vLimit 1 $ hLimit 1 $ str "#"
+    row = center $ hBox (intersperse vBorder (replicate 8 cell))
+    cell = vhLimit v h $ str "#"
 
 main :: IO ()
 main = simpleMain ui
