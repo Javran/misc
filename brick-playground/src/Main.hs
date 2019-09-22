001cf4b9f7e8a892e21c38f36c95f6f1f225f033
Javran Cheng <Javran.c@gmail.com>
2019-09-22 00:00:16 -0700

    rendering with actual states.

--- brick-playground/src/Main.hs
+++ brick-playground/src/Main.hs
@@ -3,9 +3,10 @@ module Main
   ) where
 
 import Brick
-import Brick.Widgets.Center
 import Brick.Widgets.Border
+import Brick.Widgets.Center
 import Data.List
+import Graphics.Vty.Attributes (defAttr)
 
 vhLimit :: Int -> Int -> Widget a -> Widget a
 vhLimit v h = vLimit v . hLimit h
@@ -17,10 +18,10 @@ type Coord = (Int, Int)
 toWidgetPos :: Int -> Int -> Coord -> Location
 toWidgetPos v h (r,c) = Location (2 + c*(h+1), 2 + r*(v+1))
 
-ui :: Int -> Int -> Widget RName
-ui v h =
+ui :: Int -> Int -> Coord -> Widget RName
+ui v h coord =
   joinBorders $ center
-    $ showCursor RName (toWidgetPos v h (2,4))
+    $ showCursor RName (toWidgetPos v h coord)
     $ vhLimit fullV fullH grid
   where
     (fullV, fullH) = ((v+1)*8+1, (h+1)*8+1)
@@ -38,8 +39,13 @@ ui v h =
 
 main :: IO ()
 main = do
-  let app = (simpleApp (ui 1 1))
-        { appChooseCursor = const $ showCursorNamed RName
+  let app =
+        App
+        { appDraw = \s -> [ui 1 1 s]
+        , appHandleEvent = resizeOrQuit
+        , appStartEvent = pure
+        , appAttrMap = const $ attrMap defAttr []
+        , appChooseCursor = const $ showCursorNamed RName
         }
       {-
         TODO: for now let's set state as coordinate,
