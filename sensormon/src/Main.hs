85be0f880d3b26c5536557a0cb9662e4ffd3cd31
Javran Cheng <Javran.c@gmail.com>
2019-10-24 23:40:07 -0700

    a bit investigation on field naming.

--- sensormon/src/Main.hs
+++ sensormon/src/Main.hs
@@ -14,6 +14,22 @@ import Data.Aeson
 import qualified Data.Text as T
 import qualified Data.ByteString.Lazy as BSL
 
+{-
+  the corresponding object looks like:
+
+  - tempX_input
+  - tempX_max
+  - tempX_crit
+
+  In which X is an integer. sensors seems to simply group them by X,
+  so for parsing individual ones it is fine to just ignore it.
+ -}
+data TempInfo
+  = TempInfo
+  { tiInput :: Int
+  , tiMax :: Maybe Int
+  , tiCrit :: Maybe Int
+  }
 
 {-
   This program parses output from `sensors` and print out useful info
@@ -24,6 +40,12 @@ import qualified Data.ByteString.Lazy as BSL
  -}
 toText' :: FilePath -> T.Text
 toText' = either id id . toText
+
+{-
+  naming here is a bit confusing, the following doc looks authoritative on this topic:
+
+  https://www.kernel.org/doc/Documentation/hwmon/sysfs-interface
+ -}
 
 main :: IO ()
 main = do
