3b19550585c8c459c9135f424a9cdd258089ba31
Javran Cheng <Javran.c@gmail.com>
2019-10-24 22:47:26 -0700

    sensormon kickoff.

---
+++ sensormon/src/Main.hs
@@ -0,0 +1,15 @@
+module Main
+  ( main
+  ) where
+
+{-
+  This program parses output from `sensors` and print out useful info
+  regarding temperature readings.
+
+  Eventually this will end up being info source to some xmonad components.
+
+  TODO: only skeleton. impl pending.
+ -}
+
+main :: IO ()
+main = pure ()
