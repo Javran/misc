f3ca1f9727b3510230a9f8682638a2c600505484
Javran Cheng <Javran.c@gmail.com>
2019-10-24 23:20:47 -0700

    prog exec and JSON parsing.

--- sensormon/src/Main.hs
+++ sensormon/src/Main.hs
@@ -1,6 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main
   ( main
   ) where
+
+import Prelude hiding (FilePath)
+
+import Filesystem.Path.CurrentOS
+import System.Exit
+import Turtle.Prelude
+import Data.Text.Encoding (encodeUtf8)
+import Data.Aeson
+
+import qualified Data.Text as T
+import qualified Data.ByteString.Lazy as BSL
+
 
 {-
   This program parses output from `sensors` and print out useful info
@@ -8,8 +21,14 @@ module Main
 
   Eventually this will end up being info source to some xmonad components.
 
-  TODO: only skeleton. impl pending.
  -}
+toText' :: FilePath -> T.Text
+toText' = either id id . toText
 
 main :: IO ()
-main = pure ()
+main = do
+  Just sensorsBinPath <- which "sensors"
+  (ExitSuccess, rawOut) <- procStrict (toText' sensorsBinPath) ["-j"] ""
+  let parsed :: Object
+      Right parsed = eitherDecode' . BSL.fromStrict . encodeUtf8 $ rawOut
+  print parsed
