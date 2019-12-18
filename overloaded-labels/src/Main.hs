b9e0aa1493c37a57d5813cfcd06eea78d3e927cf
Javran Cheng <Javran.C@gmail.com>
2019-12-17 21:35:01 -0800

    few more stuff to play with.
    namely Point2, which proves that the "Has" typeclass is not
    necessary. and having the Has -> IsLabel is kinda trouble.

--- overloaded-labels/src/Main.hs
+++ overloaded-labels/src/Main.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE
     OverloadedLabels
-  , KindSignatures
   , DataKinds
   , FunctionalDependencies
   , FlexibleInstances
+  , FlexibleContexts
   , ScopedTypeVariables
+  , TypeFamilies
   #-}
 module Main
   ( main
@@ -20,11 +21,13 @@ import GHC.TypeLits
   - https://gitlab.haskell.org/ghc/ghc/wikis/records/overloaded-record-fields/overloaded-labels
   - The following example is modified from:
     https://downloads.haskell.org/~ghc/latest/docs/html/users_guide/glasgow_exts.html#extension-OverloadedLabels
+
  -}
 
 data Label (l :: Symbol) = Get
 
 data Point = Point Int Int deriving Show
+data Point2 = Point2 Int Int deriving Show
 
 class Has a l b | a l -> b where
   from :: a -> Label l -> b
@@ -47,9 +50,34 @@ instance Has Point "distance" Double where
 {-
   All of those above has nothing to do with Labels until the following one
   connects the dots:
+
+  This is a bit annoying here: we have to replace a with Point because
+  instance searching does not take into account constraints,
+  so we end up finding this instance even if we are searching for something for Point2.
  -}
-instance Has a l b => IsLabel l (a -> b) where
+instance Has Point l b => IsLabel l (Point -> b) where
+  {-
+    here we need to define "fromLabel @l", the type is implicit, and the explicit form should be:
+
+    fromLabel @l = \x -> from x (Get :: Label l)
+
+   -}
   fromLabel x = from x (Get :: Label l)
+
+{-
+  Another example to show that it doesn't have to be a function.
+ -}
+instance IsLabel "stuff" String where
+  fromLabel = "fff"
+
+instance IsLabel "x" (Point2 -> Int) where
+  fromLabel (Point2 x _) = x
+
+instance IsLabel "y" (Point2 -> Int) where
+  fromLabel (Point2 _ y) = y
+
+instance IsLabel "distance" (Point2 -> Double) where
+  fromLabel (Point2 x y) = sqrt (fromIntegral $ x*x + y*y)
 
 example :: Int
 example = #x (Point 1 2)
@@ -65,4 +93,7 @@ example = #x (Point 1 2)
   ==> 5.0
  -}
 main :: IO ()
-main = print (#distance (Point 3 4))
+main = do
+  print (#distance (Point 3 4))
+  print (#distance (Point2 6 8) :: Double)
+  print (#stuff :: String)
