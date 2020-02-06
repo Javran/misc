1b342a4bbfdf80304a99a434cbf15816bba1dac5
Javran Cheng <Javran.C@gmail.com>
2020-02-06 01:04:29 -0800

    stuff that works.

--- dhall-playground/src/Main.hs
+++ dhall-playground/src/Main.hs
@@ -1,6 +1,27 @@
+{-# LANGUAGE DeriveGeneric, OverloadedStrings #-}
 module Main
   ( main
   ) where
 
+import Dhall
+
+data E2
+  = E2
+    { u :: Vector Natural
+    , t :: Vector Text
+    } deriving (Generic, Show)
+
+data Example
+  = Example
+    { one :: Natural
+    , str :: Text
+    , nest :: E2
+    } deriving (Generic, Show)
+
+instance Interpret E2
+instance Interpret Example
+
 main :: IO ()
-main = pure ()
+main = do
+  x <- input auto "./confspec.dhall"
+  print (x :: Example)
