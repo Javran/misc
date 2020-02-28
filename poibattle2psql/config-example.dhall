37991a82d1930a683b70c2bbb50e2b8b1ce7c53f
Javran Cheng <Javran.C@gmail.com>
2020-02-27 23:11:39 -0800

    read config & establish db connection.

---
+++ poibattle2psql/config-example.dhall
@@ -0,0 +1,12 @@
+let sqlConfig =
+      { host = "localhost"
+      , port = 6655
+      , user = "ywwuyi"
+      , password = "sgnb"
+      , database = "kcdatabase"
+      }
+in  { pcSqlConfig = sqlConfig
+    , pcBattleDataPath = "/home/ywwuyi/.config/poi/battle-detail"
+    , pcTableName = "battle_data"
+    }
+
--- poibattle2psql/src/Main.hs
+++ poibattle2psql/src/Main.hs
@@ -1,22 +1,45 @@
+{-# LANGUAGE LambdaCase #-}
 module Main
   ( main
   ) where
 
+import Data.Text.Encoding (encodeUtf8)
 import Dhall
+import Hasql.Connection
 import System.Environment
 import System.Exit
 
+import qualified Data.Text as Text
+
 import Config
 
+acquireFromConfig :: PsqlConfig -> IO Connection
+acquireFromConfig (PsqlConfig hst pt u pw db) =
+    acquire sqlSettings >>= \case
+      Left e -> do
+        putStrLn "error while connecting to database."
+        print e
+        exitFailure
+      Right conn -> pure conn
+  where
+    sqlSettings =
+      settings
+        (encodeUtf8 hst)
+        (fromIntegral pt)
+        (encodeUtf8 u)
+        (encodeUtf8 pw)
+        (encodeUtf8 db)
+
 main :: IO ()
-main = do
-  args <- getArgs
-  case args of
-    [configPath] -> do
-      pConf@ProgConfig
-        { pcSqlConfig = sqlConfig
-        } <- inputFile auto configPath
-      pure ()
-    _ -> do
-      putStrLn "pb2psql <config.dhall>"
-      exitFailure
+main = getArgs >>= \case
+  [configPath] -> do
+    pConf@ProgConfig
+      { pcSqlConfig = sqlConfig
+      } <- inputFile auto configPath
+    conn <- acquireFromConfig sqlConfig
+    putStrLn "connection acquired successfully."
+    putStrLn "releasing connection ..."
+    release conn
+  _ -> do
+    putStrLn "pb2psql <config.dhall>"
+    exitFailure
