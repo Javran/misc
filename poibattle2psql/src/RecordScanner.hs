c095f7a61642b5b32ccce78c852d69e8fd98d53c
Javran Cheng <Javran.C@gmail.com>
2020-02-29 23:51:33 -0800

    include zlib and decompression logic.

--- poibattle2psql/src/RecordScanner.hs
+++ poibattle2psql/src/RecordScanner.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, TypeApplications #-}
 module RecordScanner where
 
 import Control.Exception
@@ -7,14 +7,17 @@ import Data.String
 import Filesystem.Path.CurrentOS
 import PostgreSQL.Binary.Data
 import Prelude hiding (FilePath)
+import System.IO hiding (FilePath)
 import Turtle.Pattern
-import Turtle.Prelude
+import Turtle.Prelude hiding (stderr)
 import Turtle.Shell
 
-import qualified Data.Aeson as Aeson
+import qualified Codec.Compression.GZip as GZ
 import qualified Control.Foldl as Foldl
-import qualified Prelude (FilePath)
+import qualified Data.Aeson as Aeson
+import qualified Data.ByteString.Lazy as BSL
 import qualified Data.Text as T
+import qualified Prelude (FilePath)
 
 toText' :: FilePath -> T.Text
 toText' = either id id . toText
@@ -39,5 +42,18 @@ data BattleRecord
   , brPacket :: [Aeson.Value]
   }
 
+loadAndDecompress' :: Prelude.FilePath -> IO BSL.ByteString
+loadAndDecompress' fp = do
+  h <- openFile fp ReadMode
+  raw <- BSL.hGetContents h
+  -- data for a record is relatively tiny, I'm fine with decompressing it all in-memory.
+  let x = BSL.toStrict $ GZ.decompress raw
+  x `seq` hClose h
+  pure $ BSL.fromStrict x
+
+loadAndDecompress :: Prelude.FilePath -> IO (Either SomeException BSL.ByteString)
+loadAndDecompress fp =
+  catch @SomeException (Right <$> loadAndDecompress' fp) (pure . Left)
+
 loadBattleRecord :: FilePath -> IO (Either SomeException BattleRecord)
-loadBattleRecord = error "TODO"
+loadBattleRecord fp = error "TODO"
