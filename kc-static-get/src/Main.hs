862014e28ca177bb0c07c3a941498b7066791f0f
Javran Cheng <Javran.C@gmail.com>
2020-03-16 23:56:35 -0700

    some TODOs as next step.

--- kc-static-get/src/Main.hs
+++ kc-static-get/src/Main.hs
@@ -19,6 +19,35 @@ import Spritesmith
 
 import qualified Config
 
+{-
+  TODO:
+  - Include a base output directory in the config file.
+    output path will be:
+
+    {baseOutputPath}/{version}/{resourcePath}
+
+    e.g.
+    /kcs2/img/common/common_itemicons.json?version=4.5.3.0
+
+    becomes:
+    {baseOutputPath}/4.5.3.0/kcs2/img/common/common_itemicons.json
+
+    if this resource is a bundle of some other resources
+    (in this case, json+png describes that bundle)
+    we'll have a directory with '.extract' as suffix. e.g.:
+
+    {baseOutputPath}/4.5.3.0/kcs2/img/common/common_itemicons.extract/
+
+    Note that if version is not specified, it is default to string "current"
+    and we always download the resource overwriting what's existing.
+
+  - After we have the base output directory in config file,
+    the binary simply accepts two args: <path to config> <resource>.
+
+    where <resource> can be a url with version number, or a local file.
+
+ -}
+
 main :: IO ()
 main = do
   args <- getArgs
