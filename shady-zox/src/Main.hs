a7af24d8b6889afbda32122538fc562b78558462
Javran Cheng <Javran.C@gmail.com>
2020-03-29 23:19:59 -0700

    download and decode.

--- shady-zox/src/Main.hs
+++ shady-zox/src/Main.hs
@@ -1,6 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main
   ( main
   ) where
 
+import Network.HTTP.Client
+import Network.HTTP.Client.TLS (tlsManagerSettings)
+import System.Environment
+
+import qualified Data.ByteString.Base64.Lazy as B64L
+
 main :: IO ()
-main = pure ()
+main = do
+  [addr] <- getArgs
+  manager <- newManager tlsManagerSettings
+
+  request <- parseRequest addr
+  response <- httpLbs request manager
+
+  let raw = responseBody response
+  -- one of them must apply.
+  print $ B64L.decode raw
+  print $ B64L.decode $ raw <> "="
+  print $ B64L.decode $ raw <> "=="
