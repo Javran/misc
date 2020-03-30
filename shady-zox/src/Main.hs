8456880e7d1e21b76f397ff1840bb15ab3e23b9a
Javran Cheng <Javran.C@gmail.com>
2020-03-29 23:58:28 -0700

    somewhat more details.

--- shady-zox/src/Main.hs
+++ shady-zox/src/Main.hs
@@ -12,6 +12,15 @@ import qualified Data.ByteString.Lazy as BSL
 import qualified Data.ByteString.Base64.Lazy as B64L
 import qualified Data.Text as T
 
+processRawSsrLines :: BSL.ByteString -> IO ()
+processRawSsrLines raw = do
+  putStrLn "++++"
+  print raw
+  let g@[host,port,protocol,method,obfs,left] =
+        BSLC.split ':' . B64L.decodeLenient . BSLC.drop 6 $ raw
+  print g
+  putStrLn "----"
+
 main :: IO ()
 main = do
   [addr] <- getArgs
@@ -25,4 +34,4 @@ main = do
         -- lenient mode adds padding for us so we don't have to deal with it.
         . B64L.decodeLenient
         $ raw
-  mapM_ print rawSsrLines
+  mapM_ processRawSsrLines rawSsrLines
