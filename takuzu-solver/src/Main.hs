660db248dbe8ac441f2b2e829aa689fd0385d215
Javran Cheng <Javran.C@gmail.com>
2020-04-06 23:52:17 -0700

    boilerplates, get it started.

---
+++ takuzu-solver/src/Main.hs
@@ -0,0 +1,22 @@
+module Main
+  ( main
+  ) where
+
+exampleRaw :: [] ([] Char)
+exampleRaw =
+  [ "    rr  br  "
+  , "      r  b b"
+  , "  br    r  b"
+  , " r r        "
+  , "b     r b b "
+  , "  b b     b "
+  , " r  br  r   "
+  , "r    r      "
+  , "r   r   bb  "
+  , "  r     b   "
+  , "      r   rb"
+  , "  r  r      "
+  ]
+
+main :: IO ()
+main = pure ()
