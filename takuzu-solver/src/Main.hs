49d612853e8907ae50d2c66648d982356311bc0c
Javran Cheng <Javran.C@gmail.com>
2020-04-07 23:07:56 -0700

    implement some basic functions.
    include table generation and basic matching facilities.

--- takuzu-solver/src/Main.hs
+++ takuzu-solver/src/Main.hs
@@ -2,13 +2,23 @@
   Solver for https://en.wikipedia.org/wiki/Takuzu
 
   Credit to game "0h h1" by Q42 for introducting me to this game.
-  If you see a lot of mention of color red and blue instead
+  If you see a lot of mention of color blue and red instead
   of 0 and 1, that's why.
-  When packing to binary bits, red is 0 and blue is 1.
+
+  The decision that blue=0 and red=1 is made because 'b' < 'r' alphabetically,
+  this allows us to accept any raw table with ' ' and two more other characters
+  in it, and assign 0,1 to these two characters in order.
+  e.g. "101 " will result in the exact same input representation as "rbr ".
  -}
+{-# LANGUAGE
+    RoleAnnotations
+  #-}
 module Main
   ( main
   ) where
+
+import qualified Data.Vector.Unboxed as VU
+import qualified Data.Set as S
 
 exampleRaw :: [] ([] Char)
 exampleRaw =
@@ -26,26 +36,66 @@ exampleRaw =
   , "  r  r      "
   ]
 
+type Cell = Bool
+
+cBlue, cRed :: Cell
+[cBlue, cRed] = [False, True]
+
+-- NOTE: more compact representation can be achieved through a bit vector impl,
+-- or just Word64 or something shorter
+-- since we've never planned to solve a large scale problem
+-- by preprocessing a large table.
+-- but for now let's focus on correctness first.
+type CompleteLine = VU.Vector Cell
+
 {-
   Total number of valid cell placements in a single line
   can be calculated following https://oeis.org/A177790
+
+  "Digits" are generated backwards since that's the most efficient way,
+  this does not effect correctness given this problem's symmetric nature.
  -}
-gen 0 0 xs = [xs]
-gen rCount bCount xs = case xs of
+genLineAux :: Int -> Int -> [Bool] -> [] [Bool]
+genLineAux 0 0 xs = [xs]
+genLineAux rCount bCount xs = case xs of
     x0:x1:_
       | x0 == x1 ->
-        if x0 == 'r'
-          then newB
-          else newR
+        if x0
+          then newR
+          else newB
     _ -> newB <> newR
   where
     newB = do
       True <- [bCount > 0]
-      gen rCount (bCount-1) ('b' : xs)
+      genLineAux rCount (bCount-1) (cRed : xs)
     newR = do
       True <- [rCount > 0]
-      gen (rCount-1) bCount ('r' : xs)
+      genLineAux (rCount-1) bCount (cBlue : xs)
+
+mkTable :: Int -> [] CompleteLine
+mkTable n = VU.fromListN (n+n)  <$> genLineAux n n []
+
+-- match a complete line against a partial line.
+matchLine :: CompleteLine -> [Maybe Cell] -> Bool
+matchLine cl inp = and $ zipWith matches (VU.toList cl) inp
+  where
+    matches _ Nothing = True
+    matches b0 (Just b1) = b0 == b1
+
+-- extra common features from lines.
+-- input must be non-empty, and all elements are assumed to have the same length.
+summarizeLines :: [] CompleteLine -> [] (S.Set Cell)
+summarizeLines ls = extractInd <$> [0 .. size-1]
+  where
+    extractInd :: Int -> S.Set Cell
+    extractInd i = S.fromList ((VU.! i) <$> ls)
+    size = VU.length (head ls)
 
 main :: IO ()
 main = do
-  mapM_ (\c -> print . length $ gen c c []) [1,2,3,4,5,6]
+  let tbl = mkTable 2
+      ln = [Just cBlue, Just cRed, Nothing, Nothing]
+      r0 = filter (flip matchLine ln) tbl
+      r1 = summarizeLines r0
+  print r0
+  print r1
