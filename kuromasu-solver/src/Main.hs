3dd2d460ef2f97b33bfd94a5d34deabe1cbf3502
Javran Cheng <Javran.C@gmail.com>
2020-04-14 22:50:13 -0700

    generate placements.

--- kuromasu-solver/src/Main.hs
+++ kuromasu-solver/src/Main.hs
@@ -2,6 +2,9 @@ module Main
   ( main
   ) where
 
+import Control.Monad
+import Data.List
+
 import qualified Data.Map.Strict as M
 import qualified Data.Set as S
 
@@ -39,5 +42,49 @@ data Board
     , bdCandidates :: M.Map Coord [M.Map Coord Cell]
     }
 
+{-
+  The following type defines a building block for building "blueprint"s given a number
+  and the original coordinate.
+
+  Assume a cell is at (0,0), Placement 1 2 3 0 will mean the following setup:
+
+  - (0,0): blue
+  - (-1,0): blue, (-2,0): red
+  - (0,1) & (0,2): blue, (0,3): red
+  - (1,0) & (2,0) & (3,0): blue, (4,0): red
+  - (0,-1): red
+
+  note that we have negative numbers, which is fine since we are only building the blueprint,
+  but the generated candidates (i.e. sets of coord-color pairs) will be less at runtime.
+
+  also note that there's another way of eliminating an candidate: if it's dimension is too long
+  to be fit in a board (e.g. a 9x9 board can have maximum number of 16,
+  but a 1x17 Placement is simply impossible to fit). This kind of elimination can be done when building up the blueprint.
+
+ -}
+data Placement =
+  Placement Int Int Int Int {- up, right, down, left. in this order -}
+  deriving Show
+
+{-
+  pick up items in that order. one item can be pick up multiple times.
+ -}
+pickInOrder' :: [a] -> [] (a,[a])
+pickInOrder' = fmap (\(x:xs) -> (x,x:xs)) . init . tails
+
+gen _ _ 0 cur = pure [cur]
+gen (rows, cols) mods todoCount cur = do
+  (f, mods') <- pickInOrder' mods
+  let cur'@(Placement u r d l) = f cur
+  guard $ u + d < rows && l + r < cols
+  gen (rows, cols) mods' (todoCount-1) cur'
+
 main :: IO ()
-main = pure ()
+main = do
+  let mods =
+        [ \(Placement u r d l) -> Placement (u+1) r d l
+        , \(Placement u r d l) -> Placement u (r+1) d l
+        , \(Placement u r d l) -> Placement u r (d+1) l
+        , \(Placement u r d l) -> Placement u r d (l+1)
+        ]
+  mapM_ print $ gen (9,9) mods 16 (Placement 0 0 0 0)
