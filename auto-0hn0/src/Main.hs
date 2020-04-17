5e5a66ce8b3262c44d77aeb7ec2c81c8155aa5ec
Javran Cheng <Javran.C@gmail.com>
2020-04-16 21:35:44 -0700

    collect samples.

--- auto-0hn0/src/Main.hs
+++ auto-0hn0/src/Main.hs
@@ -16,6 +16,7 @@ import System.Random.Shuffle
 
 import qualified Data.Map.Strict as M
 import qualified Graphics.Image as HIP
+import qualified Graphics.Image.IO as HIP
 import qualified Data.ByteString as BS
 import qualified Graphics.Image.Processing.Binary as HIP
 
@@ -35,7 +36,15 @@ screenCapture = do
   pure img
 
 main :: IO ()
-main = pure ()
-
-
-
+main = do
+  imgFull <- screenCapture
+  let samples :: [((Int, Int), Image)]
+      samples =
+        concat
+        . (fmap . fmap) (\coord@(r,c') -> (coord, HIP.crop (413+111*r+30, c'+37) (39,28) imgFull))
+        $ [ [ (r,c) | c <- [41,152,263,374,486,598,710,822,934]] | r <- [0..8] ]
+  forM_ samples $ \((r,c), img) ->
+    let fName = "samples/sample_" <> show r <> "_" <> show c <> ".png"
+    in HIP.writeImageExact HIP.PNG [] fName img
+  -- _ <- getLine
+  pure ()
