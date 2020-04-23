2f98638c0283c6d435ffd235c3e9c430d4c9e6d9
Javran Cheng <Javran.C@gmail.com>
2020-04-22 23:57:24 -0700

    separate recvProto as well.

--- ma-client/src/MaClientMain.hs
+++ ma-client/src/MaClientMain.hs
@@ -40,6 +40,19 @@ sendProto sock msg = do
   sendAll sock encodedLen
   sendAll sock encoded
 
+recvProto :: Message msg => Socket -> IO msg
+recvProto sock = do
+  [r0, r1, r2, r3] <- fmap (fromIntegral @Word8 @Word32) . BS.unpack <$> recvAtLeast sock 4
+  let responseSize =
+        fromLittleEndian
+        $ r0 .|. (r1 `shiftL` 8) .|. (r2 `shiftL` 16) .|. (r3 `shiftL` 24)
+  responseRaw <- recvAtLeast sock (fromIntegral responseSize)
+  let responseM = decodeMessage responseRaw
+  case responseM of
+    Left err -> error $ "decode error: " <> err
+    Right msg ->
+      pure msg
+
 main :: IO ()
 main = do
   [fp] <- getArgs
@@ -47,28 +60,11 @@ main = do
   let hints = defaultHints { addrSocketType = Stream }
       req :: MA.FindTagRequest
       req = defMessage & MA.payload .~ raw
-      encoded = encodeMessage req
-      len :: Word32
-      len = toLittleEndian . fromIntegral $ BS.length encoded
-      encodedLen =
-        BS.pack
-        . fmap (fromIntegral @Word32 @Word8 . (.&. 0xFF) )
-        $ [len, len `shiftR` 8, len `shiftR` 16, len `shiftR` 24]
   addr:_ <- getAddrInfo (Just hints) (Just "127.0.0.1") (Just "17151")
   sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
   connect sock $ addrAddress addr
   sendProto sock req
-  putStrLn $ "Sent raw: " <> show (BS.length raw)
-  [r0, r1, r2, r3] <- fmap (fromIntegral @Word8 @Word32) . BS.unpack <$> recvAtLeast sock 4
-  let responseSize =
-        fromLittleEndian
-        $ r0 .|. (r1 `shiftL` 8) .|. (r2 `shiftL` 16) .|. (r3 `shiftL` 24)
-  responseRaw <- recvAtLeast sock (fromIntegral responseSize)
-  let responseM :: Either String MA.FindTagResponse
-      responseM = decodeMessage responseRaw
-  case responseM of
-    Left err -> putStrLn $ "Decode error: " <> err
-    Right msg ->
-      putStrLn . showMessage $ msg
+  msg <- recvProto @MA.FindTagResponse sock
+  putStrLn . showMessage $ msg
   close sock
   pure ()
