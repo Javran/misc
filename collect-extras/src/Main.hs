8b4e0817d35289613725677cf12555d493142249
Javran Cheng <Javran.C@gmail.com>
2020-05-21 23:31:35 -0700

    few explanations.

--- collect-extras/src/Main.hs
+++ collect-extras/src/Main.hs
@@ -45,11 +45,23 @@ type C = Compose (Writer [String]) (Writer [Int])
 
 wTest :: C ()
 wTest = Compose $ do
+  -- action for the "outside" applicative.
   tell ["log0"]
   tell ["log1"]
-  pure (tell [12,34] >> tell [45] :: Writer [Int] ())
+  {-
+    action for the "inside" applicative.
+    Note that, unlike monad transformer stacks,
+    we are intentionally not interleaving actions
+    but layering them out: outside and then pure inside.
+
+    This limits each compose action to do only one step,
+    but those compose actions can be composed, allowing a limited form
+    of interleaving.
+   -}
+  pure
+    (tell [12,34] >> tell [45] :: Writer [Int] ())
 
 main :: IO ()
 main = do
-  let v = runWriter $ getCompose wTest
+  let v = runWriter $ getCompose (wTest *> wTest)
   print v
