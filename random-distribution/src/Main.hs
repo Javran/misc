8f325ae9a1352da360c535f87708877bed20169b
Javran Cheng <Javran.C@gmail.com>
2020-08-04 23:29:53 -0700

    binary instance for SeedPack.

--- random-distribution/src/Main.hs
+++ random-distribution/src/Main.hs
@@ -7,12 +7,21 @@ where
 
 import Control.Monad
 import Control.Monad.Trans
+import Data.Binary
 import Data.Random
 import Data.Random.Source.MWC
 import qualified Data.Vector as V
 import qualified Data.Vector.Mutable as VM
 
 -- experiment to generate different random distributions
+newtype SeedPack = SeedPack (V.Vector Word32)
+
+spLen :: Int
+spLen = 256
+
+instance Binary SeedPack where
+  put (SeedPack vs) = mapM_ put vs
+  get = (SeedPack . V.fromListN spLen) <$> replicateM spLen get
 
 -- a Double is generated and multipled by `scale` to give range from 0 to scale-1
 scaledExperiment :: Int -> Int -> IO (V.Vector Int)
