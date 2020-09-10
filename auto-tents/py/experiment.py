773296bbf41a6a42c09cad01b5e9d4e9b7dd2032
Javran Cheng <Javran.C@gmail.com>
2020-09-09 23:38:28 -0700

    attempt to scale things manually with 14x14 sample.

--- auto-tents/py/experiment.py
+++ auto-tents/py/experiment.py
@@ -7,26 +7,36 @@
 import cv2
 from matplotlib import pyplot
 
-def find_and_mark_matches(img, result, pat_dims):
+def find_and_mark_matches(img, result, pat_dims, mx):
     h, w = result.shape
     pat_h, pat_w = pat_dims
+    threshold = 0.9995 * mx
     for r in range(h):
         for c in range(w):
-            if (result[r,c] > 0.9995):
+            if (result[r,c] > threshold):
                 print(r,c, result[r,c])
                 top_left = (c,r)
                 bottom_right = (c + pat_w, r + pat_h)
                 cv2.rectangle(img,top_left, bottom_right, 255, 2)
 
 def main():
+  scale = 83 / 211
+  # width of the original pattern: 211
   pat = cv2.imread('../sample/tree-sample.png')
+
+  pat_w = int(pat.shape[1] * scale)
+  pat_h = int(pat.shape[0] * scale)
+
+  pat = cv2.resize(pat, (pat_w, pat_h))
+
   print(pat.shape)
-  pat_h, pat_w, _ = pat.shape
-  img = cv2.imread('../private/sample-5x5.png')
+  # pat_h, pat_w, _ = pat.shape  # for 14x14, the target width seems to be 83
+  img = cv2.imread('../private/sample-14x14.png')
+
   result = cv2.matchTemplate(img,pat,cv2.TM_CCORR_NORMED)
   result_norm = cv2.normalize(result,0, 255)
-  find_and_mark_matches(img, result, [pat_h, pat_w])
   min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+  find_and_mark_matches(img, result, [pat_h, pat_w], max_val)
   print(f'min: {min_val}, max: {max_val}')
   top_left = max_loc
   bottom_right = (top_left[0] + pat_w, top_left[1] + pat_h)
