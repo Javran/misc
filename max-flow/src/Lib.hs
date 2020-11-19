5528b192da813692b8b514f1f3573f015dac7cf2
Javran Cheng <Javran.C@gmail.com>
2020-11-18 23:48:52 -0800

    include another example file.
    from: https://github.com/mbuzdalov/papers/blob/master/2015-cec-flows/flows/tests/handmade/wash-t10.dimacs

--- max-flow/src/Lib.hs
+++ max-flow/src/Lib.hs
@@ -1,19 +1,22 @@
 module Lib
   ( main
-  ) where
+  )
+where
 
-import Parser
-import Paths_max_flow
 import qualified Data.Text.IO as T
 import EdmondsKarp
+import Parser
+import Paths_max_flow
 
 main :: IO ()
 main = do
-  fnData <- getDataFileName "data/simple.dimacs"
+  fnData <-
+    getDataFileName
+      -- "data/simple.dimacs"
+      "data/wash-t10.dimacs.txt"
   raw <- T.readFile fnData
   let Right nr = parseFromRaw raw
   print nr
   let (result, logs) = maxFlow nr
-  print result
   mapM_ T.putStrLn logs
-  pure ()
+  print result
