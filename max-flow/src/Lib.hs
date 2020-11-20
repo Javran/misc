de9d942b39d967941dc05da62d9c47e2f1f662b1
Javran Cheng <Javran.C@gmail.com>
2020-11-19 20:13:12 -0800

    load data file at run time.

--- max-flow/src/Lib.hs
+++ max-flow/src/Lib.hs
@@ -6,18 +6,20 @@ where
 import qualified Data.Text.IO as T
 import EdmondsKarp
 import Parser
-import Paths_max_flow
+import System.Environment
+import System.Exit
 
 main :: IO ()
 main = do
-  fnData <-
-    getDataFileName
-    -- "data/simple.dimacs"
-    -- "data/wash-t10.dimacs.txt"
-    "data/wikipedia.dimacs"
-  raw <- T.readFile fnData
-  let Right nr = parseFromRaw raw
-  print nr
-  let (result, logs) = maxFlow nr
-  mapM_ T.putStrLn logs
-  print result
+  args <- getArgs
+  case args of
+    [fName] -> do
+      raw <- T.readFile fName
+      let Right nr = parseFromRaw raw
+      print nr
+      let (result, logs) = maxFlow nr
+      mapM_ T.putStrLn logs
+      print result
+    _ -> do
+      putStrLn "<prog> <data file>"
+      exitFailure
