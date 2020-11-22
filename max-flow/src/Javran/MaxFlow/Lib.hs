2f71898cecf343e3646d6954f386b1dd80f51bc6
Javran Cheng <Javran.C@gmail.com>
2020-11-21 23:37:01 -0800

    some basic work to support processing batches.

--- max-flow/src/Javran/MaxFlow/Lib.hs
+++ max-flow/src/Javran/MaxFlow/Lib.hs
@@ -7,14 +7,28 @@ import qualified Data.Map.Strict as M
 import qualified Data.Text.IO as T
 import Javran.MaxFlow.EdmondsKarp
 import Javran.MaxFlow.Parser
+import System.Directory
 import System.Environment
 import System.Exit
+import System.FilePath.Posix
+
+batchProcess :: FilePath -> IO ()
+batchProcess pBase = do
+  de <- doesDirectoryExist pBase
+  if de
+    then
+      listDirectory pBase
+        >>= mapM_ (batchProcess . (pBase </>))
+    else do
+      -- TODO: process a file
+      print pBase
+      pure ()
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [fName] -> do
+    ["dev", fName] -> do
       raw <- T.readFile fName
       case parseFromRaw raw of
         Left msg -> do
@@ -34,6 +48,14 @@ main = do
                 "non zero assignments: "
                   <> show
                     (filter ((/= 0) . snd) $ M.toList arcs)
+    ["batch", basePath] -> do
+      p <- doesPathExist basePath
+      if p
+        then batchProcess basePath
+        else do
+          putStrLn "path does not exist"
+          exitFailure
     _ -> do
-      putStrLn "<prog> <data file>"
+      putStrLn "<prog> dev <data file>: run on a single dimacs file."
+      putStrLn "<prog> batch <base path>: batch-process a directory of files"
       exitFailure
