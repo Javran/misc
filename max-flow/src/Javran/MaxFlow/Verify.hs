c96be9c4bf5c2db356a70be4e7a70890d3508e71
Javran Cheng <Javran.C@gmail.com>
2020-11-25 00:15:32 -0800

    finish verification on node net flow.

--- max-flow/src/Javran/MaxFlow/Verify.hs
+++ max-flow/src/Javran/MaxFlow/Verify.hs
@@ -4,10 +4,11 @@ import Control.Monad
 import Control.Monad.Except
 import qualified Data.IntMap.Strict as IM
 import qualified Data.Map.Strict as M
+import Data.Monoid
 import Javran.MaxFlow.Types
 
-verify :: CapacityMap -> Flow -> Except String ()
-verify cMap fl = do
+verify :: Int -> Int -> CapacityMap -> Flow -> Except String ()
+verify sourceNode sinkNode cMap fl = do
   {-
     verify edges, make sure capacities are within correct ranges,
     and every arc is assigned a capacity (even if it is assigned 0)
@@ -24,5 +25,20 @@ verify cMap fl = do
           Nothing ->
             throwError $ "arc " <> show (u, v) <> " is not assigned."
   {-
-    TODO: all nodes should have a net-flow of 0 except source and sink.
+    TODO: we should verify on CapacityMap that nothing flows into source and nothing flows out of sink.
    -}
+  let allNodeNets = foldr go (IM.map (const 0) cMap) (M.toList fl)
+        where
+          incrMap k diff = IM.alter (\(Just x) -> Just (x <> Sum diff)) k
+          go ((u, v), c) m =
+            if c == 0
+              then m
+              else incrMap u (- c) . incrMap v c $ m
+      srcNet = allNodeNets IM.! sourceNode
+      sinkNet = allNodeNets IM.! sinkNode
+      nodeNets = IM.delete sourceNode $ IM.delete sinkNode allNodeNets
+  when (srcNet + sinkNet /= 0) $
+    throwError "unbalanced source and sink net flow."
+  forM_ (IM.toList nodeNets) $ \(u, c) ->
+    when (c /= 0) $
+      throwError $ "node " <> show u <> " has a non-zero net flow of value " <> show c
