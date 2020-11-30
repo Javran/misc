30db94fcfeb5b2f8d4dcff17a3fbe09342d5ef2c
Javran Cheng <Javran.C@gmail.com>
2020-11-29 23:58:32 -0800

    getArc wrapper around M.

--- max-flow/src/Javran/MaxFlow/Dinitz.hs
+++ max-flow/src/Javran/MaxFlow/Dinitz.hs
@@ -1,7 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Javran.MaxFlow.Dinitz () where
 
+import Control.Monad.Except
+import Control.Monad.RWS ()
+import Control.Monad.Trans.Except ()
+import Control.Monad.Trans.RWS.CPS
+import Control.Monad.Trans.Writer.CPS
+import qualified Data.DList as DL
 import qualified Data.IntMap.Strict as IM
 import qualified Data.Map.Strict as M
+import Data.Monoid
+import qualified Data.Text as T
 import Javran.MaxFlow.Types
 
 {-
@@ -12,11 +22,21 @@ import Javran.MaxFlow.Types
 
 type RInfo = (NetworkRep, CapacityMap)
 
+type M =
+  RWST
+    (NetworkRep, CapacityMap)
+    (Sum Int)
+    Flow
+    ( ExceptT
+        String
+        (Writer (DL.DList T.Text))
+    )
+
 {-
   Lookup current flow value and capacity of an arc.
  -}
-lookupArc :: RInfo -> Flow -> (Int, Int) -> Maybe (Int, Int)
-lookupArc (_, cMap) fl p@(u, v) = do
+lookupArc :: CapacityMap -> Flow -> (Int, Int) -> Maybe (Int, Int)
+lookupArc cMap fl p@(u, v) = do
   subCMap <- cMap IM.!? u
   cap <- subCMap IM.!? v
   {-
@@ -29,3 +49,21 @@ lookupArc (_, cMap) fl p@(u, v) = do
           then - (fl M.! (v, u))
           else fl M.! p
   pure (cur, cap)
+
+logM :: T.Text -> M ()
+logM t =
+  lift $
+    lift $
+      Control.Monad.Trans.Writer.CPS.tell $
+        DL.singleton t
+
+getArc :: (Int, Int) -> M (Int, Int)
+getArc p = do
+  cMap <- asks snd
+  fl <- get
+  case lookupArc cMap fl p of
+    Just v -> pure v
+    Nothing -> do
+      let msg = "lookup failed for edge " <> show p
+      logM (T.pack msg)
+      lift $ throwError msg
