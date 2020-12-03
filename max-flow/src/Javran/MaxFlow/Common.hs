b1293d4ae52a5b07c19016331618db72baed0661
Javran Cheng <Javran.C@gmail.com>
2020-12-02 23:53:02 -0800

    move prepare function out.
    this normalization process is not a part of Edmonds-Karps and can be
    shared between different algorithms.

--- max-flow/src/Javran/MaxFlow/Common.hs
+++ max-flow/src/Javran/MaxFlow/Common.hs
@@ -1,15 +1,21 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NamedFieldPuns #-}
 
 module Javran.MaxFlow.Common
   ( normalize
   , getNR
   , NormalizedNetwork
+  , prepare
   )
 where
 
+import Control.Monad.Except
+import qualified Data.IntMap.Strict as IM
 import qualified Data.Map.Strict as M
 import Data.Maybe
 import Data.Monoid
+import qualified Data.Set as S
 import Javran.MaxFlow.Types
 
 newtype NormalizedNetwork = NormalizedNetwork
@@ -53,3 +59,57 @@ normalize nr@NetworkRep {nrArcs} =
             Nothing
           LT -> Just (p, Sum v)
           GT -> Just ((y, x), Sum (- v))
+
+-- TODO: should accept normalized network
+prepare :: NetworkRep -> Either String (CapacityMap, Flow)
+prepare NetworkRep {nrArcCount, nrArcs, nrNodeCount} = runExcept $ do
+  let szArcs = length nrArcs
+  unless (szArcs == nrArcCount) $
+    throwError "arc count mismatched."
+  let checkArc ((src, dst), cap) = do
+        {-
+          by making sure that capacity is positive in the original graph,
+          we can be sure whenever an arc (u,v) with 0 capacity shows up,
+          the actual flow goes in the opposite direction.
+          (i.e. the actual flow is from v to u with a positive capacity)
+         -}
+        unless (cap > 0) $
+          throwError "capacity must be positive."
+        {-
+          check each arc and make sure they are not self-linking (src == dst)
+          and is within range.
+         -}
+        when (src == dst) $
+          throwError "arc src and dst should not be the same"
+        unless (src > 0 && src <= nrNodeCount) $
+          throwError "invalid arc src node"
+        unless (dst > 0 && dst <= nrNodeCount) $
+          throwError "invalid arc src node"
+  mapM_ checkArc nrArcs
+  let allArcs :: [((Int, Int), Int)]
+      allArcs = nrArcs <> fmap revArc nrArcs
+        where
+          revArc ((src, dst), _) = ((dst, src), 0)
+      initFlow = M.fromList $ fmap (\(p, _) -> (p, 0)) nrArcs
+      capa :: CapacityMap
+      capa = foldr go IM.empty allArcs
+        where
+          go ((src, dst), cap) =
+            IM.alter
+              (\case
+                 Nothing -> Just $ IM.singleton dst cap
+                 Just m -> Just $ IM.insert dst cap m)
+              src
+  {-
+    for each pair (u,v) we expect it to be unique and (v,u) to not be
+    a part of the same network. If this expectation is not met,
+    an error will be raised.
+   -}
+  unless (getSum (foldMap (Sum . IM.size) capa) == nrArcCount * 2) $ do
+    let pairs = S.fromList $ fmap ((\p@(x, y) -> if x <= y then p else (y, x)) . fst) nrArcs
+    throwError $
+      "capacity map size mismatch: unique: "
+        <> show (S.size pairs)
+        <> ", total: "
+        <> show szArcs
+  pure (capa, initFlow)
