adeb8fda7b525dd9308d3991442a2a0bc7f052a6
Javran Cheng <Javran.C@gmail.com>
2021-01-10 23:11:40 -0800

    utils for benchmarks

--- max-flow/bench/Bench.hs
+++ max-flow/bench/Bench.hs
@@ -4,6 +4,40 @@ module Bench
 where
 
 import Criterion.Main
+import Data.Bifunctor
+import qualified Data.ByteString as BS
+import Data.List
+import Data.Text.Encoding (decodeUtf8)
+import Javran.MaxFlow.Common
+import Javran.MaxFlow.Parser
+import Javran.MaxFlow.TestData
+import Javran.MaxFlow.Types
+
+pSimple, pGenetic, pHandmade, pRandomBest :: [(String, NormalizedNetwork)]
+[ pSimple
+  , pGenetic
+  , pHandmade
+  , pRandomBest
+  ] =
+    fmap
+      normPack
+      [ packSimple
+      , packGenetic
+      , packHandmade
+      , packRandomBest
+      ]
+    where
+      normPack :: [(FilePath, BS.ByteString)] -> [(FilePath, NormalizedNetwork)]
+      normPack = sortOn fst . fmap (second norm)
+        where
+          norm :: BS.ByteString -> NormalizedNetwork
+          norm raw = normalize nr
+            where
+              Right nr = parseFromRaw $ decodeUtf8 raw
+
+runSolver :: MaxFlowSolver -> [(String, NormalizedNetwork)] -> [(String, (Int, Flow, CapacityMap))]
+runSolver solver =
+  (fmap . second) (\nn -> let (Right r, _) = solver nn in r)
 
 main :: IO ()
 main = defaultMain []
