0c8f5975509f1883ba22f2f83f69b2e8ed1d0d18
Javran Cheng <Javran.C@gmail.com>
2021-01-22 22:58:42 -0800

    prepare extra states.

--- max-flow/src/Javran/MaxFlow/Algorithm/Karzanov.hs
+++ max-flow/src/Javran/MaxFlow/Algorithm/Karzanov.hs
@@ -6,6 +6,7 @@ import Control.Monad.Except
 import Control.Monad.State
 import qualified Data.IntMap.Strict as IM
 import qualified Data.Map.Strict as M
+import Data.Maybe
 import qualified Data.Set as S
 import Javran.MaxFlow.Algorithm.Internal
 import Javran.MaxFlow.Common
@@ -45,7 +46,7 @@ type Frozens = S.Set (Int, Int)
 data Extras = Extras
   { eIns :: InStack
   , eOuts :: OutList
-  , eFronzens :: Frozens
+  , eFrozens :: Frozens
   }
 
 -- This solver is the standard one plus an extra layer of Extras as StateT
@@ -63,5 +64,12 @@ prepare' nr@NetworkRep {nrSource} = runExcept $ do
       fl =
         -- set all outgoing edges to its capacity in the preflow.
         M.union (M.fromList srcOutArcs) fl0
-  -- TOOD: prepare extras.
-  pure (cMap, fl, undefined)
+      eIns = IM.insert nrSource srcOutArcs (IM.map (const []) cMap)
+      eOuts = IM.fromList $
+        catMaybes $ do
+          (u, subMap) <- IM.toList cMap
+          let vs :: [Int]
+              vs = IM.keys $ IM.filter (> 0) subMap
+          (x : xs) <- pure vs
+          pure (Just (u, ((x, False), xs)))
+  pure (cMap, fl, Extras {eIns, eOuts, eFrozens = mempty})
