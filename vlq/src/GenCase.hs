c296123945bea29093307d3cd062fd8be6a21ba9
Javran Cheng <Javran.C@gmail.com>
2021-02-26 21:35:28 -0800

    done qc checks.

--- vlq/src/GenCase.hs
+++ vlq/src/GenCase.hs
@@ -101,7 +101,7 @@ testRawUrl =
   "https://raw.githubusercontent.com/exercism/problem-specifications/\
   \main/exercises/variable-length-quantity/canonical-data.json"
 
-newtype PprHex a = PprHex [a] deriving (IsList, Eq)
+newtype PprHex a = PprHex [a] deriving (IsList, Eq, Foldable)
 
 type family DisplayWidth i where
   DisplayWidth Word32 = 8
--- vlq/test/VlqSpec.hs
+++ vlq/test/VlqSpec.hs
@@ -1,32 +1,60 @@
+{-# LANGUAGE BinaryLiterals #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE TypeFamilies #-}
 
 module VlqSpec where
 
 import Cases
 import Control.Monad
+import qualified Data.Bits as Bits
 import Data.Coerce
 import qualified Data.Text as T
+import Data.Word
 import GenCase
 import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
 import Vlq
 
 spec :: Spec
 spec = do
-  let mkSpec label testCases testFunc =
-        describe label $
-          forM_ testCases $
-            \( Case
-                 { description
-                 , inputAndExpected = (input, expected)
-                 }
-               ) -> do
-                specify (T.unpack description) $
-                  (coerce testFunc) input `shouldBe` expected
-  mkSpec "encode" encodeTests encodes
-  mkSpec "decode" decodeTests decodes
-  -- TODO: fail when too many bits
-  -- TODO: encodes sets top bit correctly
-  -- TODO: encodes then decodes is identity
-  -- TODO: decodes then encodes is identity
+  let encodes' :: PprHex Word32 -> PprHex Word8
+      encodes' = coerce encodes
+      decodes' :: PprHex Word8 -> Either DecodeError (PprHex Word32)
+      decodes' = coerce decodes
+      mkCases testCases testFunc =
+        forM_ testCases $
+          \( Case
+               { description
+               , inputAndExpected = (input, expected)
+               }
+             ) -> do
+              specify (T.unpack description) $
+                testFunc input `shouldBe` expected
+  describe "encodes" $ do
+    prop "encoded result is non-empty" $
+      \x -> not (null (encodes' [x]))
+    prop "top bits are set correctly" $
+      \x ->
+        let PprHex encoded = encodes' [x]
+            mask = 0b1000_0000
+         in all ((/= 0) . (Bits..&. mask)) (init encoded)
+              .&&. ((last encoded Bits..&. mask) == 0)
+    mkCases encodeTests encodes'
+
+  describe "decodes" $ do
+    mkCases decodeTests decodes'
+    prop "octet length of 5 but with too many bits" $ do
+      lo <- choose (0, 0b0111_1111)
+      mids <- replicateM 3 $ choose (0b1000_0000, 0b1111_1111)
+      hi <- choose (0b1001_1111, 0b1111_1111)
+      pure $ decodes' (PprHex ([hi] <> mids <> [lo])) === Left TooManyBits
+
+  describe "properties" $ do
+    prop "decodes . encodes ~ id" $
+      forAll
+        (PprHex <$> listOf (choose (minBound, maxBound)))
+        (\xs -> (decodes' . encodes') xs === Right xs)
