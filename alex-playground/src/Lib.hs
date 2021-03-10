dc99d58a751628936812a9586e22647ddadea928
Javran Cheng <Javran.C@gmail.com>
2021-03-09 23:40:34 -0800

    stuff.

--- alex-playground/src/Lib.hs
+++ alex-playground/src/Lib.hs
@@ -27,6 +27,7 @@ import Token
   Latest spec seems to be:
 
   - https://docs.oracle.com/javase/specs/jls/se15/html/index.html
+  - https://docs.oracle.com/javase/specs/jls/se15/html/jls-3.html
  -}
 
 {-
