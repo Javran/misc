c4439c5ba1afbf733e229f2852846a5a78311c95
Javran Cheng <Javran.C@gmail.com>
2021-03-10 21:29:09 -0800

    some new project ideas that might worth a try.

---
+++ st/src/Lib.hs
@@ -0,0 +1,16 @@
+module Lib
+  ( main
+  ) where
+
+{-
+  WIP: project idea: a tool that does all boring stack routines:
+
+  - remove junks from stack.yaml
+  - sort and dedup dependencies in package.yaml
+  - check what is the lastest lts compatible with certain version of GHC
+  - find project home by going upwards and run commands with PROJECT_HOME set.
+
+ -}
+
+main :: IO ()
+main = pure ()
