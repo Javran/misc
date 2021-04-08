2b78ab74d3a8a9a9001e6201e8c598e523c4e5c5
Javran Cheng <Javran.C@gmail.com>
2021-04-07 23:54:48 -0700

    quickcheck property test.

--- gaussian-elim/test/LibSpec.hs
+++ gaussian-elim/test/LibSpec.hs
@@ -1,13 +1,23 @@
 {-# LANGUAGE TypeApplications #-}
+
 module LibSpec where
 
-import Test.Hspec
-import Test.QuickCheck
 import Lib
+import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
+import Test.QuickCheck.Gen
 
 spec :: Spec
 spec =
   describe "extEuclidean" $ do
     specify "example" $
       extEuclidean @Integer 1234 4147 `shouldBe` (1, (-1314, 391))
-
+    prop "props" $ do
+      x <- choose (1, 0xFFFFFF)
+      y <- choose (1, 0xFFFFFF)
+      let (t, (u, v)) = extEuclidean @Integer x y
+      pure $
+        t === gcd x y
+          .&&. (gcd x y =/= 1
+                  .||. u * x + v * y === 1)
