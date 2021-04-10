ef569770e322f2fd6e6950aa11e45fa38310f0ea
Javran Cheng <Javran.C@gmail.com>
2021-04-10 00:01:37 -0700

    test hexagon parsing.

--- gaussian-elim/test/ParserSpec.hs
+++ gaussian-elim/test/ParserSpec.hs
@@ -5,8 +5,8 @@ import Test.Hspec
 
 spec :: Spec
 spec =
-  describe "fromRawString" $
-    specify "examples" $
+  describe "fromRawString" $ do
+    specify "example (square 4)" $
       fromRawString
         (unlines
            [ "4"
@@ -25,5 +25,32 @@ spec =
                 , [0, 0, 3, 3]
                 , [3, 2, 3, 0]
                 , [3, 0, 2, 0]
+                ]
+            }
+    specify "example (hexagon 4)" $
+      fromRawString
+        (unlines
+           [ "6"
+           , "hexagon 4"
+           , "1 3 5 1"
+           , "4 5 5 4 4"
+           , "0 3 2 1 2 0"
+           , "5 2 1 4 2 3 5"
+           , "0 1 2 1 4 5"
+           , "3 4 2 2 2"
+           , "3 4 0 3"
+           ])
+        `shouldBe` Just
+          Puzzle
+            { opMod = 6
+            , pzType = PHexagon 4
+            , grid =
+                [ [1, 3, 5, 1]
+                , [4, 5, 5, 4, 4]
+                , [0, 3, 2, 1, 2, 0]
+                , [5, 2, 1, 4, 2, 3, 5]
+                , [0, 1, 2, 1, 4, 5]
+                , [3, 4, 2, 2, 2]
+                , [3, 4, 0, 3]
                 ]
             }
