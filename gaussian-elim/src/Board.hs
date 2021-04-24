5ec0fc4fa0bdf066ec3b6c5451a10e3adb3de16b
Javran Cheng <Javran.C@gmail.com>
2021-04-23 22:57:02 -0700

    improve interface.

--- gaussian-elim/src/Board.hs
+++ gaussian-elim/src/Board.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE ExplicitForAll #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies #-}
 
 module Board where
@@ -14,71 +15,57 @@ data PuzzleShape
 
 class CoordSystem (k :: PuzzleShape) where
   type Coord k
-  getCoords :: forall p. p k -> Int -> ([[Int]], [[Coord k]])
   surrounding :: forall p. p k -> Coord k -> [Coord k]
+  shapedCoords :: forall p. p k -> Int -> [[Coord k]]
 
 instance CoordSystem 'Square where
   type Coord 'Square = (Int, Int)
-  getCoords _ = sqCoords
-  surrounding _ = sqSurrounding
+  surrounding _ (x, y) = do
+    i <- [x -1 .. x + 1]
+    j <- [y -1 .. y + 1]
+    pure (i, j)
 
-sqCoords sz = (fmap mkEqn allCoords, nestedAllCoords)
-  where
-    mkEqn c =
-      fmap (\c' -> if c' `elem` xs then 1 else 0) allCoords
-      where
-        xs = coordEqns M.! c
-
-    coordEqns = M.fromDistinctAscList $ fmap (\c -> (c, surrounding' c)) allCoords
-
-    nestedAllCoords = [[(r, c) | c <- [0 .. sz -1]] | r <- [0 .. sz -1]]
-    allCoords = concat nestedAllCoords
-    allCoords' = S.fromDistinctAscList allCoords
-    surrounding' (x, y) = do
-      c <- sqSurrounding (x, y)
-      c <$ guard (S.member c allCoords')
-
-sqSurrounding (x, y) = do
-  i <- [x -1 .. x + 1]
-  j <- [y -1 .. y + 1]
-  pure (i, j)
+  shapedCoords _ sz = [[(r, c) | c <- [0 .. sz -1]] | r <- [0 .. sz -1]]
 
 type CubeCoord = (Int, Int, Int)
 
 instance CoordSystem 'Hexagon where
   type Coord 'Hexagon = CubeCoord
-  getCoords _ = hexCoords
-  surrounding _ = hexSurrounding
+  surrounding _ c@(x, y, z) =
+    c :
+    [ (x, y + 1, z -1)
+    , (x, y -1, z + 1)
+    , (x + 1, y, z -1)
+    , (x -1, y, z + 1)
+    , (x + 1, y -1, z)
+    , (x - 1, y + 1, z)
+    ]
 
-hexCoords :: Int -> ([[Int]], [[CubeCoord]])
-hexCoords sz = (fmap mkEqn allCoords, nestedAllCoords)
+  shapedCoords _ sz =
+    [[(x, y, z) | y <- [3, 2 .. (-3 - z)], let x = - y - z] | z <- [- mx .. 0]]
+      <> [[(x, y, z) | x <- [-3 .. 3 - z], let y = - x - z] | z <- [1 .. mx]]
+    where
+      mx = sz -1
+
+gCoords
+  :: forall p cs.
+  (CoordSystem cs, Ord (Coord cs))
+  => p cs
+  -> Int
+  -> ([[Int]], [[Coord cs]])
+gCoords ty sz = (fmap mkEqn allCoords, nestedAllCoords)
   where
-    mx = sz -1
-    nestedAllCoords :: [[CubeCoord]]
-    nestedAllCoords =
-      [[(x, y, z) | y <- [3, 2 .. (-3 - z)], let x = - y - z] | z <- [- mx .. 0]]
-        <> [[(x, y, z) | x <- [-3 .. 3 - z], let y = - x - z] | z <- [1 .. mx]]
+    nestedAllCoords :: [[Coord cs]]
+    nestedAllCoords = shapedCoords ty sz
     allCoords = concat nestedAllCoords
     allCoords' = S.fromList allCoords
-    surrounding' c@(x, y, z) =
+    surrounding' c =
       filter
         (`S.member` allCoords')
-        $ hexSurrounding c
-    coordEqns :: M.Map CubeCoord [CubeCoord]
+        $ surrounding ty c
+    coordEqns :: M.Map (Coord cs) [Coord cs]
     coordEqns = M.fromList $ fmap (\c -> (c, surrounding' c)) allCoords
-    mkEqn :: CubeCoord -> [Int]
     mkEqn c =
       fmap (\c' -> if c' `elem` xs then 1 else 0) allCoords
       where
-        xs :: [CubeCoord]
         xs = coordEqns M.! c
-
-hexSurrounding c@(x, y, z) =
-  c :
-  [ (x, y + 1, z -1)
-  , (x, y -1, z + 1)
-  , (x + 1, y, z -1)
-  , (x -1, y, z + 1)
-  , (x + 1, y -1, z)
-  , (x - 1, y + 1, z)
-  ]
