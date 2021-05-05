0a5bbdcf76ec97ee0d163567a779471093210f2c
Javran Cheng <Javran.C@gmail.com>
2021-05-04 23:16:22 -0700

    impl logistic equation.

--- dynamical-systems/src/Unit_3.hs
+++ dynamical-systems/src/Unit_3.hs
@@ -3,5 +3,12 @@ module Unit_3
   )
 where
 
+import Control.Monad
+
+logisticIter r = iterate next
+  where
+    next x = r * x * (1 - x)
+
 main :: IO ()
-main = pure ()
+main = forM_ (zip [0 .. 20] (logisticIter 3.7 0.9)) $ \(i, v) ->
+  putStrLn $ show i <> ": " <> show v
