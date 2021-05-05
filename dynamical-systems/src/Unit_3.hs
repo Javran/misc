d209b6cb3867a213812ca58683a281c60862c016
Javran Cheng <Javran.C@gmail.com>
2021-05-04 23:22:22 -0700

    a bunch of TODOs.

--- dynamical-systems/src/Unit_3.hs
+++ dynamical-systems/src/Unit_3.hs
@@ -9,6 +9,13 @@ logisticIter r = iterate next
   where
     next x = r * x * (1 - x)
 
+
+{-
+  TODO:
+
+  (a) two different initial condition and diff between them.
+  (b) display symbolic sequence, and calculate frequency of LLL, LLR, LRL and so on.
+ -}
 main :: IO ()
 main = forM_ (zip [0 .. 20] (logisticIter 3.7 0.9)) $ \(i, v) ->
   putStrLn $ show i <> ": " <> show v
