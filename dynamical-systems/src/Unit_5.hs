d843371c215ef41f2e2bbe54f0388dc672eefc13
Javran Cheng <Javran.C@gmail.com>
2021-05-06 21:30:25 -0700

    support plotting a specific region.

--- dynamical-systems/src/Unit_5.hs
+++ dynamical-systems/src/Unit_5.hs
@@ -3,10 +3,10 @@ module Unit_5
   )
 where
 
+import Control.Monad
 import qualified Data.Set as S
 import System.Console.Terminfo
 import Text.Printf
-import Control.Monad
 
 type SkipThenKeep = (Int, Int)
 
@@ -19,14 +19,20 @@ collectPoints (s, k) =
 getSequence :: Double -> Double -> [Double]
 getSequence r seed = iterate f seed
   where
-    f x = r * x * (1-x)
+    f x = r * x * (1 - x)
 
-renderLine :: Int -> S.Set Double -> String
-renderLine cols xs = fmap tr [0..cols-1]
+renderLine :: Int -> (Double, Double) -> S.Set Double -> String
+renderLine cols (xLo, xHi) xs = fmap tr [0 .. cols -1]
   where
+    xDist = xHi - xLo
     cols' = fromIntegral cols
     markedCols :: S.Set Int
-    markedCols = S.map (\v -> round (cols' * v)) xs
+    markedCols =
+      S.fromList
+        . fmap (\x -> round (cols' * (x - xLo) / xDist))
+        . filter (\x -> x >= xLo && x <= xHi)
+        . S.toList
+        $ xs
     tr c = if S.member c markedCols then '*' else ' '
 
 main :: IO ()
@@ -34,11 +40,11 @@ main = do
   term <- setupTermFromEnv
   let Just (rows, cols) = getCapability term ((,) <$> termLines <*> termColumns)
   printf "rows: %d, cols: %d\n" rows cols
-  let (rLo, rHi) = (2.88, 4.0)
+  let (rLo, rHi) = (3.536511882631319, 3.593323318496883)
+      xRange = (0.7765042979942693, 0.9140401146131805)
       wantRows = rows - 3
       step = (rHi - rLo) / fromIntegral (wantRows -1)
-      -- t = collectPoints (100,200) $ getSequence 3.7206 0.5
-  forM_ (take wantRows [rLo,rLo+step..]) $ \r -> do
-    let t = collectPoints (100,200) $ getSequence r 0.5
-    putStrLn $ renderLine cols t
+  forM_ (take wantRows [rLo, rLo + step ..]) $ \r -> do
+    let t = collectPoints (100, 200) $ getSequence r 0.5
+    putStrLn $ renderLine cols xRange t
   pure ()
