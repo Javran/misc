fce2bdda55bd8195bdc90ceb8dc672d6778e4d55
Javran Cheng <Javran.C@gmail.com>
2021-05-25 22:54:08 -0700

    try to bring up a server
    looks working.

--- kancolle-cache-server/src/KcCacheServer/Main.hs
+++ kancolle-cache-server/src/KcCacheServer/Main.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeApplications, OverloadedStrings #-}
 
 module KcCacheServer.Main where
 
@@ -11,15 +11,21 @@ import qualified Data.Text as T
 import Dhall
 import qualified KcCacheServer.CacheMeta as R
 import qualified KcCacheServer.Config as Config
+import Network.Wai.Handler.Warp
+import Network.Wai
 import System.Environment
 import System.Exit
 import System.FilePath.Posix
+import Network.HTTP.Types
+
+app :: Application
+app req respond = respond $ responseLBS status200 [] "foo"
 
 main :: IO ()
 main =
   getArgs >>= \case
     configFp : _extraArgs -> do
-      _cfg@Config.Config {Config.cacheBase} <- inputFile @Config.Config auto configFp
+      cfg@Config.Config {Config.cacheBase} <- inputFile @Config.Config auto configFp
       r <-
         eitherDecodeFileStrict
           @(HM.HashMap T.Text R.ResourceMeta)
@@ -27,8 +33,9 @@ main =
       case r of
         Left err -> error err
         Right parsed -> do
-          let s = S.fromList . fmap R.lastModified $ HM.elems parsed
-          print s
+          let _s = S.fromList . fmap R.lastModified $ HM.elems parsed
+          -- print s
+          run (fromIntegral $ Config.port $ Config.local cfg) app
           pure ()
     _ -> do
       putStrLn "<prog> <server config> [args...]"
