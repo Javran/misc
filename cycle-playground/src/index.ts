e76c66c74c61d85453ae8c8e50a238ff447b737c
Javran Cheng <Javran.C@gmail.com>
2021-05-28 18:22:20 -0700

    run eject.
    don't fucking introduce a layer of abstract
    if that layer doesn't work in the first place.
    I don't know how this is a template that everything is
    fucking broken that user has to fix their shit.

--- cycle-playground/src/index.ts
+++ cycle-playground/src/index.ts
@@ -1,8 +1,8 @@
-import { run } from '@cycle/run';
-import { getDrivers, wrapMain } from './drivers';
-import { Component } from './interfaces';
-import { App } from './components/app';
+import { run } from '@cycle/run'
+import { getDrivers, wrapMain } from './drivers'
+import { Component } from './interfaces'
+import { App } from './components/app'
 
-const main: Component<any> = wrapMain(App);
+const main: Component<any> = wrapMain(App)
 
-run(main as any, getDrivers());
+run(main as any, getDrivers())
