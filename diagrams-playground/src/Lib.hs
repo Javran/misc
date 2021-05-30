9d2056a38ded89cd873e5af87c5c06888eaec0c0
Javran Cheng <Javran.C@gmail.com>
2021-05-30 02:22:48 -0700

    to directly move things to places.

--- diagrams-playground/src/Lib.hs
+++ diagrams-playground/src/Lib.hs
@@ -8,6 +8,7 @@ where
 
 import Diagrams.Angle
 import Diagrams.Backend.Cairo.CmdLine
+-- import Diagrams.Backend.SVG.CmdLine
 import Diagrams.Prelude
 
 main :: IO ()
@@ -27,10 +28,26 @@ cairoUnit =
      # snugT)
     # center
 
-example :: Diagram Cairo
-example =
+exampleFourCompo :: Diagram B
+exampleFourCompo =
   (((cairoUnit # snugR <> ((cairoUnit # alignY 1) # snugL)) # snugL
-   <> (cairoUnit # alignY 1) # snugR) # center) # snugB <> snugT cairoUnit
+      <> (cairoUnit # alignY 1) # snugR)
+     # center)
+    # snugB
+    <> snugT cairoUnit
+
+example :: Diagram B
+example =
+  atPoints
+    [ p2 (0, 0)
+    , p2 (hf, - hf)
+    , p2 (- hf, - hf)
+    , p2 (0, -2 * hf)
+    ]
+    (repeat cairoUnit)
+  where
+    hf :: Double
+    hf = height (cairoUnit :: D V2 Double) / 2
 
 -- cairoPen :: Diagram Cairo
 cairoPen =
