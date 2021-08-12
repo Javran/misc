b80d0c723646823be8ee8fb921550536d048becf
Javran Cheng <Javran.C@gmail.com>
2021-08-11 23:42:29 -0700

    basic stuff.

--- chess/src/Lib.hs
+++ chess/src/Lib.hs
@@ -1,6 +1,25 @@
 module Lib
   ( main
-  ) where
+  )
+where
+
+import Data.Word
+
+newtype Bitboard
+  = Bitboard Word64
+
+{-
+  pawns and kings might not be considered pieces,
+  but let's not make it more complicated than needed.
+ -}
+data PieceType
+  = Pawn
+  | Knight
+  | Bishop
+  | Rook
+  | Queen
+  | King
+  deriving (Enum, Eq, Ord)
 
 main :: IO ()
 main = pure ()
