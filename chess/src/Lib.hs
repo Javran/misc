8938c2484a9ea07e8c0cf97fba3bde00db3eb7e4
Javran Cheng <Javran.C@gmail.com>
2021-08-11 23:58:37 -0700

    indexing into halfboard.

--- chess/src/Lib.hs
+++ chess/src/Lib.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DerivingVia #-}
+
 module Lib
   ( main
   )
@@ -6,8 +8,11 @@ where
 import qualified Data.Vector.Unboxed as VU
 import Data.Word
 
-newtype Bitboard
-  = Bitboard Word64
+{-
+  TODO: use newtype ideally - having trouble here as Vector Word64 and Vector Bitboard
+  are not exactly the same.
+ -}
+type Bitboard = Word64
 
 {-
   pawns and kings might not be considered pieces,
@@ -26,6 +31,9 @@ data PieceType
   A halfboard contains exactly 6 elements for 6 piece types.
  -}
 newtype Halfboard = Halfboard (VU.Vector Bitboard)
+
+hbAt :: Halfboard -> PieceType -> Bitboard
+hbAt (Halfboard hb) pt = hb VU.! fromEnum pt
 
 {-
   (<white side>, <black side>)
