d89e9da8dde2f14b09a38fb66b3a62a6f6f4b195
Javran Cheng <Javran.C@gmail.com>
2021-08-14 16:44:27 -0700

    bind to a tmp var.

--- chess/src/Game/Chess/TH.hs
+++ chess/src/Game/Chess/TH.hs
@@ -3,7 +3,13 @@ module Game.Chess.TH where
 import Language.Haskell.TH
 
 destructList :: Int -> (Int -> String) -> Q Exp -> Q [Dec]
-destructList l varName rhsQ = do
+destructList l varName rhsE = do
   vs <- mapM (varP . mkName . varName) [0 .. l -1]
-  rhs <- rhsQ
-  pure [ValD (ListP vs) (NormalB rhs) []]
+  rhsN <- newName "rhs"
+  rhs <- rhsE
+  pure
+    [ ValD
+        (ListP vs)
+        (NormalB (VarE rhsN))
+        [ValD (VarP rhsN) (NormalB rhs) []]
+    ]
