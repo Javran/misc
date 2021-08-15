5937f692acf4457265ff5275e02454f5a23b70c9
Javran Cheng <Javran.C@gmail.com>
2021-08-14 17:13:32 -0700

    better name and doc, probably.

--- chess/src/Game/Chess/TH.hs
+++ chess/src/Game/Chess/TH.hs
@@ -4,12 +4,16 @@ import Control.Monad
 import Language.Haskell.TH
 
 {-
-  Note: it would be nice to have type signatures on those names automatically,
-  but we don't have that type info for arbitrary expressions.
- -}
+  Usage: `bindList l varName <exp> <element type>`
 
-destructList :: Int -> (Int -> String) -> Q Exp -> Q Type -> Q [Dec]
-destructList l varName rhsQ tyQ = do
+  Expects a list of exactly `l` elements, bind them to variables
+  and declare their type signatures.
+
+  Note: it would be nice to have type signatures on those names automatically,
+  but we might not have the type info at hand when doing TH.
+ -}
+bindList :: Int -> (Int -> String) -> Q Exp -> Q Type -> Q [Dec]
+bindList l varName rhsQ tyQ = do
   let ns = fmap (mkName . varName) [0 .. l -1]
   vs <- mapM (varP . mkName . varName) [0 .. l -1]
   rhs <- rhsQ
