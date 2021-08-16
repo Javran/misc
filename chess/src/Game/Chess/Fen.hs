20eab0959fe067e9b3c2386ef9b0137a05aa6e24
Javran Cheng <Javran.C@gmail.com>
2021-08-15 23:56:37 -0700

    parsing of a rank.

--- chess/src/Game/Chess/Fen.hs
+++ chess/src/Game/Chess/Fen.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE TypeApplications #-}
+
 module Game.Chess.Fen where
 
 import Control.Applicative
 import Data.Attoparsec.ByteString.Char8
+import Data.Char
 import Data.Monoid
 import Data.Word
 import Game.Chess.Coord
@@ -23,10 +26,28 @@ data Record = Record
 rawStandardBoard = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
 pElemP :: Parser (Sum Word8, [Maybe (Color, PieceType)])
-pElemP = "Pp" ~> Pawn
+pElemP =
+  choice
+    [ "Pp" ~> Pawn
+    , "Nn" ~> Knight
+    , "Bb" ~> Bishop
+    , "Rr" ~> Rook
+    , "Qq" ~> Queen
+    , "Kk" ~> King
+    , do
+        c <- satisfy (\ch -> ch >= '1' && ch <= '8')
+        let cnt = ord c - ord '0'
+        pure (Sum (fromIntegral cnt), replicate cnt Nothing)
+    ]
   where
-    (~>) :: [Char] -> PieceType -> Parser (Sum Word8, [Maybe (Color, PieceType)])
     [wRaw, bRaw] ~> pt = do
-      color <- (White <$ char wRaw) <|> (Black <$ char bRaw)
+      color <-
+        (White <$ char wRaw)
+          <|> (Black <$ char bRaw)
       pure (1, [Just (color, pt)])
     _ ~> _ = error "unreachable"
+
+rankP :: Parser (EightElems (Maybe (Color, PieceType)))
+rankP = do
+  (Sum 8, es) <- mconcat <$> many1 pElemP
+  pure es
