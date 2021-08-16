df4b11e3eee05f5822136a650456039b4ebd904f
Javran Cheng <Javran.C@gmail.com>
2021-08-15 23:25:11 -0700

    Record def and parsing setup.

--- chess/src/Game/Chess/Fen.hs
+++ chess/src/Game/Chess/Fen.hs
@@ -1,7 +1,20 @@
 module Game.Chess.Fen where
 
+import Data.Attoparsec.ByteString.Char8
+import Game.Chess.Types
+
 {-
   https://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation
  -}
+
+
+data Record = Record
+  { placement :: [[Maybe (Color, PieceType)]]
+  , activeColor :: Color
+  , castling :: ([Side], [Side])
+  , enPassantTarget :: () -- TODO
+  , halfMove :: Int
+  , fullMove :: Int
+  }
 
 rawStandardBoard = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
