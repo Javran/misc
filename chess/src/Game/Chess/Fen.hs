77122b6435b086c390f9e278b1d496f8be6cc4ee
Javran Cheng <Javran.C@gmail.com>
2021-08-15 23:26:57 -0700

    EP can use just Coord.

--- chess/src/Game/Chess/Fen.hs
+++ chess/src/Game/Chess/Fen.hs
@@ -1,7 +1,9 @@
 module Game.Chess.Fen where
 
+
 import Data.Attoparsec.ByteString.Char8
 import Game.Chess.Types
+import Game.Chess.Coord
 
 {-
   https://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation
@@ -12,7 +14,7 @@ data Record = Record
   { placement :: [[Maybe (Color, PieceType)]]
   , activeColor :: Color
   , castling :: ([Side], [Side])
-  , enPassantTarget :: () -- TODO
+  , enPassantTarget :: Maybe LinearCoord
   , halfMove :: Int
   , fullMove :: Int
   }
