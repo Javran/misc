1d041bca7bbfa25303230a009ad8b79b8f4f86c5
Javran Cheng <Javran.C@gmail.com>
2021-08-29 14:39:44 -0700

    note on type rep design.

--- chess/src/Game/Chess/Board.hs
+++ chess/src/Game/Chess/Board.hs
@@ -7,6 +7,19 @@ import qualified Data.Vector.Fixed.Boxed as VFB
 import Game.Chess.Bitboard
 import Game.Chess.Types
 
+{-
+  There are multiple ways to represent a board with Bitboard as elemenets:
+
+  - we can do a vector of 12 elements
+  - or a pair of vectors of 6 elements in each
+
+  And when it comes to the choice of boxed / unboxed vector, we can do both.
+
+  For now performance doesn't matter, so let's just pick one and stick with it
+  unless there are blocking problems along the way.
+
+  I choose to use a pair of boxed vectors so to allow more sharing.
+ -}
 type Halfboard = VFB.Vec 6 Bitboard
 
 emptyHb :: Halfboard
