903d1dd9486d7e5f1aa23bd8e8f1eb2f80a6e9b4
Javran Cheng <Javran.C@gmail.com>
2021-10-23 18:33:46 -0700

    for checking presence of battle bgm.

--- kc-navy-album-maintenance/src/KcNavyAlbum/Main.hs
+++ kc-navy-album-maintenance/src/KcNavyAlbum/Main.hs
@@ -3,5 +3,20 @@ module KcNavyAlbum.Main
   )
 where
 
+import Kantour.GameResource.Magic
+import Network.HTTP.Client
+import Network.HTTP.Types
+import Control.Monad
+import Text.Printf
+
+checkBattleBgm :: Manager -> Int -> IO Int
+checkBattleBgm mgr bgmId = do
+  let code = magicCode bgmId "bgm_battle"
+  req <- parseRequest (printf "http://%s/kcs2/resources/bgm/battle/%03d_%04d.mp3" defaultServer bgmId code)
+  resp <- httpNoBody req mgr
+  pure (statusCode $ responseStatus resp)
+
 main :: IO ()
-main = pure ()
+main = do
+  mgr <- newManager defaultManagerSettings
+  print =<< forM [1..20] (checkBattleBgm mgr)
