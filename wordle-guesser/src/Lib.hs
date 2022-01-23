f4e9e81e0d1743cf653ed95421ef4beb1d92a380
Javran Cheng <Javran.C@gmail.com>
2022-01-23 03:44:23 -0800

    some random improvements.

--- wordle-guesser/src/Lib.hs
+++ wordle-guesser/src/Lib.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NamedFieldPuns #-}
 
 module Lib
@@ -7,26 +8,55 @@ module Lib
 where
 
 import Control.Monad
+import Control.Monad.State.Strict
+import Data.Char
+import Data.Either
 import Data.Foldable
-import qualified Data.IntSet as IS
-import qualified Data.Map.Strict as M
-import Data.Maybe (catMaybes)
+import Data.List
+import Data.Maybe
 import Data.Ord
-import qualified Data.Set as S
+import Debug.Trace
+import GHC.Stack.Types (HasCallStack)
+import System.Random
+import System.Random.Shuffle
+import Text.ParserCombinators.ReadP hiding (get)
 
 data Result = Result
   { rCorrects :: [Maybe Char]
-  , rWrongPlaces :: M.Map Char IS.IntSet
-  , rNotIn :: S.Set Char
+  , rWrongPlaces :: [(Char, Int)]
+  , rNotIn :: [] Char
   }
-  deriving (Show)
+  deriving (Show, Eq)
 
 makeGuess :: String -> String -> Result
 makeGuess answer guess = Result {rCorrects, rWrongPlaces, rNotIn}
   where
-    gSet = S.fromList guess
-    ansSet = S.fromList answer
     rCorrects = zipWith (\x y -> x <$ guard (x == y)) answer guess
+    corrects = catMaybes rCorrects
+    -- discharge exact matches
+    answer' = answer \\ corrects
+
+    collectStep :: State ([] Int, [] Char) (Maybe (Either Char (Char, Int)))
+    collectStep = do
+      (curWrongIndices, curAns) <- get
+      case curWrongIndices of
+        [] -> pure Nothing
+        wi : wis -> do
+          let wCh = guess !! wi
+          if wCh `elem` curAns
+            then do
+              put (wis, delete wCh curAns)
+              pure $ Just (Right (wCh, wi))
+            else do
+              put (wis, curAns)
+              pure $ Just (Left wCh)
+
+    collect acc = do
+      r <- collectStep
+      case r of
+        Nothing -> pure (partitionEithers $ reverse acc)
+        Just a -> collect (a : acc)
+    (rNotIn, rWrongPlaces) = evalState (collect []) (wrongIndices, answer')
     wrongIndices =
       catMaybes $
         zipWith
@@ -35,93 +65,108 @@ makeGuess answer guess = Result {rCorrects, rWrongPlaces, rNotIn}
              Just _ -> Nothing)
           [0 :: Int ..]
           rCorrects
-    rWrongPlaces = M.fromListWith (<>) do
-      i <- wrongIndices
-      let gCh = guess !! i
-      guard $ S.member gCh ansSet
-      pure (gCh, IS.singleton i)
-    rNotIn = gSet S.\\ ansSet
 
-couldMatch :: Result -> String -> Bool
-couldMatch Result {rCorrects, rWrongPlaces, rNotIn} candidate =
-  condCorrect && condWrongPlaces && condNotIn
-  where
-    condCorrect =
-      and $
-        zipWith
-          (\m ch -> case m of
-             Nothing -> True
-             Just v -> v == ch)
-          rCorrects
-          candidate
-    condWrongPlaces = and do
-      (w, indices) <- M.toList rWrongPlaces
-      i <- IS.toList indices
-      guard $ w `elem` candidate
-      pure (candidate !! i /= w)
-    condNotIn = not $ any (\c -> S.member c rNotIn) candidate
+couldMatch :: Result -> String -> String -> Bool
+couldMatch result guess candidate = result == makeGuess candidate guess
 
+tryElim :: [Char] -> String -> [] String -> [] String
 tryElim actualAnswer guess searchSpace = do
   let crit = makeGuess actualAnswer guess
   candidate <- searchSpace
-  guard $ couldMatch crit candidate
+  guard $ couldMatch crit guess candidate
   pure candidate
 
-{-
+data Attempted
+  = Good Char
+  | Misp Char
+  | Nope Char
 
-  TODO:
-  correct answer: crimp
-  guess: m(yellow) i(yellow) m(grey) i(grey) c(yellow)
+couldMatch' :: [Attempted] -> String -> Bool
+couldMatch' attemptedResult candidate = couldMatch r attemptedWord candidate
+  where
+    r = Result {rCorrects, rWrongPlaces, rNotIn}
+    rNotIn =
+      mapMaybe
+        (\case
+           Nope v -> Just v
+           _ -> Nothing)
+        attemptedResult
+    rWrongPlaces =
+      catMaybes $
+        zipWith
+          (\i m -> case m of
+             Misp c -> Just (c, i)
+             _ -> Nothing)
+          [0 ..]
+          attemptedResult
+    rCorrects =
+      fmap
+        (\case
+           Good c -> Just c
+           _ -> Nothing)
+        attemptedResult
+    attemptedWord =
+      fmap
+        (\case
+           Good c -> c
+           Misp c -> c
+           Nope c -> c)
+        attemptedResult
+
+consumeAllWithReadP :: ReadP a -> String -> Maybe a
+consumeAllWithReadP p xs = case readP_to_S (p <* eof) xs of
+  [(v, "")] -> pure v
+  _ -> Nothing
+
+consumeOrDie :: HasCallStack => ReadP a -> String -> a
+consumeOrDie p = fromJust . consumeAllWithReadP p
+
+attemptedP :: ReadP [Attempted]
+attemptedP = do
+  cs <- replicateM 5 (satisfy isAsciiLower)
+  _ <- between skipSpaces skipSpaces (char '|')
+  let resultP =
+        (Good <$ char 'g')
+          <++ (Misp <$ char 'm')
+          <++ (Nope <$ char 'n')
+  mk <- replicateM 5 resultP
+  pure $ zipWith ($) mk cs
+
+{-
+  Current best initial guesses:
+
+  - [("roted",247957)]
 
  -}
 main :: IO ()
 main = do
   answers <- lines <$> readFile "wordle-answers-alphabetical.txt"
   allowedGuesses <- lines <$> readFile "wordle-allowed-guesses.txt"
-  let crit0 =
-        Result
-          { rCorrects = [Nothing, Nothing, Nothing, Nothing, Nothing]
-          , rWrongPlaces = M.empty
-          , rNotIn = S.fromList "fault"
-          }
-      crit1 =
-        Result
-          { rCorrects = [Nothing, Nothing, Nothing, Nothing, Nothing]
-          , rWrongPlaces =
-              M.fromList
-                [ ('m', IS.fromList [0])
-                , ('i', IS.fromList [1])
-                , ('c', IS.fromList [4])
-                ]
-          , rNotIn = S.empty
-          }
-      crit2 =
-        Result
-          { rCorrects = [Nothing, Nothing, Nothing, Just 'm', Nothing]
-          , rWrongPlaces =
-              M.fromList
-                [ ('p', IS.fromList [0])
-                ]
-          , rNotIn = S.fromList "yg"
-          }
-      crit3 =
-        Result
-          { rCorrects = [Nothing, Nothing, Just 'i', Just 'm', Just 'p']
-          , rWrongPlaces = M.empty
-          , rNotIn = S.fromList "sk"
-          }
+  g <- newStdGen
+
+  let p = consumeOrDie attemptedP
+      _guessesExample =
+        [ p "roted | mnnnn"
+        , p "cairn | gngmn"
+        ]
+      guesses = []
+      isInitialGuess = null guesses
       searchSpace = do
         a <- answers
-        guard $ couldMatch crit0 a
-        guard $ couldMatch crit1 a
-        guard $ couldMatch crit2 a
-        -- guard $ couldMatch crit3 a
+        forM_ guesses \guess -> do
+          guard $ couldMatch' guess a
         pure a
+      allGuessSpace = answers <> allowedGuesses
+      allGuessSpaceLen = length allGuessSpace
       experiment = do
-        guess <- searchSpace
-        let alts = do
+        let guessSpace = take 40 $ shuffle' allGuessSpace allGuessSpaceLen g
+        (i, guess) <- zip [0 :: Int ..] do
+          if isInitialGuess
+            then guessSpace
+            else searchSpace
+        let alts = traceShow i do
               answer <- searchSpace
               pure (length $ tryElim answer guess searchSpace)
-        pure (guess, product (fmap fromIntegral alts :: [Integer]))
-  let (_, z) = maximumBy (comparing snd) experiment
+        pure (guess, sum (fmap fromIntegral alts :: [Integer]))
+  let (_, z) = minimumBy (comparing snd) experiment
   print $ filter ((== z) . snd) experiment
