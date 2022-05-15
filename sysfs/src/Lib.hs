70f2597446b66f65a3ff506fb99674eb76462a4e
Javran Cheng <Javran.C@gmail.com>
2022-05-15 16:20:55 -0700

    try querying dbus for inhibitors
    not really sysfs related but I want to try
    getting this info out.
    
    for now not working - for some reason
    I'm not seeing the actual inhibitor.

--- sysfs/src/Lib.hs
+++ sysfs/src/Lib.hs
@@ -6,11 +6,15 @@ module Lib
 where
 
 import qualified CapacityLevel as CL
+import DBus
+import DBus.Client
 import Data.String
+import Data.Word
 import qualified PowerStatus as PS
 import System.Directory
-import Text.ParserCombinators.ReadP
 import System.FilePath.Posix
+import Text.ParserCombinators.ReadP
+import Control.Monad
 
 {-
   For now just some power related stuff.
@@ -28,13 +32,48 @@ readAndParse what parser = do
   if b
     then do
       raw <- readFile $ defSupply </> what
-      pure $ Just $ case readP_to_S (parser <* skipSpaces <* eof) raw of
-        [(v, "")] -> Right v
-        _ -> Left raw
+      pure $
+        Just $ case readP_to_S (parser <* skipSpaces <* eof) raw of
+          [(v, "")] -> Right v
+          _ -> Left raw
     else pure Nothing
 
+type InhibitorInfo = (String, String, String, String, Word32, Word32)
+
+displayInhibitorInfo :: InhibitorInfo -> IO ()
+displayInhibitorInfo (what, who, why, mode, uid, pid) = do
+  putStrLn $ "What: " <> what
+  putStrLn $ "Who: " <> who
+  putStrLn $ "Why: " <> why
+  putStrLn $ "Mode: " <> mode
+  putStrLn $ "UID: " <> show uid
+  putStrLn $ "PID: " <> show pid
+
+{-
+  For ListInhibitors: what, who, why, mode, uid (user ID), and pid
+
+  Ref: https://www.freedesktop.org/software/systemd/man/org.freedesktop.login1.html
+ -}
 main :: IO ()
 main = do
   readAndParse "capacity_level" CL.capacityLevelP >>= print
   readAndParse "status" PS.statusP >>= print
+
+  putStrLn "# DBus"
+  sys <- connectSystem
+  reply <-
+    call_
+      sys
+      (methodCall
+         "/org/freedesktop/login1"
+         "org.freedesktop.login1.Manager"
+         "ListInhibitors")
+        { methodCallDestination = Just "org.freedesktop.login1"
+        }
+  let [r] = methodReturnBody reply
+      xs :: [InhibitorInfo]
+      Just xs = fromVariant r
+  forM_ xs $ \x -> do
+    displayInhibitorInfo x
+    putStrLn ""
   pure ()
