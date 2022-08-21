212f5e535bfe4a103e7bd3652b9f913c6ac8785c
Javran Cheng <Javran.C@gmail.com>
2022-08-20 23:00:46 -0700

    read watchlist from a file.

--- gentoo-package-watcher/exe/Main.hs
+++ gentoo-package-watcher/exe/Main.hs
@@ -1,14 +1,18 @@
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main
   ( main
-  ) where
+  )
+where
 
 import qualified Algorithms.NaturalSort
 import Control.Monad
 import Data.Aeson.Types
+import Data.Char
+import Data.Either
 import qualified Data.HashMap.Strict as HM
 import Data.List
 import Data.Ord
@@ -18,28 +22,40 @@ import qualified Javran.Gentoo.PackageWatcher.Data.Package as Pkg
 import Javran.Gentoo.PackageWatcher.Gather
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
-
-watchlist :: [Pkg.Package]
-watchlist =
-  nvidiaDrivers :
-  [ "sys-kernel/gentoo-sources"
-  , "media-video/pipewire"
-  ]
+import System.Environment
+import System.Exit
+import System.IO
 
 main :: IO ()
-main = do
-  mgr <- newManager tlsManagerSettings
-  results <- gatherAllInfo mgr watchlist
-  forM_ results \(pkg, m) -> do
-    putStrLn $ "Package: " <> show pkg
-    case m of
-      Nothing -> putStrLn "  <Fetch error>"
-      Just ebsPre ->
-        let ebs = sortOn (Data.Ord.Down . Algorithms.NaturalSort.sortKey . Eb.version) ebsPre
-         in forM_ ebs \Eb.EbuildInfo {Eb.version, Eb.extra} -> do
-              putStrLn $
-                "- " <> T.unpack version <> case extra of
-                  Nothing -> ""
-                  Just ~(Object v) ->
-                    let String kv = v HM.! "NV_KERNEL_MAX"
-                     in ", kernel max: " <> T.unpack kv
+main =
+  getArgs >>= \case
+    [fpWatchlist] -> do
+      rawWl <- readFile fpWatchlist
+      let (malforms, watchlist) =
+            partitionEithers
+              . fmap convert
+              . filter (not . all isSpace)
+              $ lines rawWl
+            where
+              convert x = case Pkg.safeFromString x of
+                Nothing -> Left x
+                Just t -> Right t
+      unless (null malforms) do
+        hPutStrLn stderr "Failed to parse the following:"
+        mapM_ (hPutStrLn stderr) malforms
+      mgr <- newManager tlsManagerSettings
+      results <- gatherAllInfo mgr watchlist
+      forM_ results \(pkg, m) -> do
+        putStrLn $ "Package: " <> show pkg
+        case m of
+          Nothing -> putStrLn "  <Fetch error>"
+          Just ebsPre ->
+            let ebs = sortOn (Data.Ord.Down . Algorithms.NaturalSort.sortKey . Eb.version) ebsPre
+             in forM_ ebs \Eb.EbuildInfo {Eb.version, Eb.extra} -> do
+                  putStrLn $
+                    "- " <> T.unpack version <> case extra of
+                      Nothing -> ""
+                      Just ~(Object v) ->
+                        let String kv = v HM.! "NV_KERNEL_MAX"
+                         in ", kernel max: " <> T.unpack kv
+    _ -> die "<prog> <path to watchlist file>"
