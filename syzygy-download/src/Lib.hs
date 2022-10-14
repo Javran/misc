214744189494112a9f9b9b6886363c2c03d3dbdf
Javran Cheng <javran@google.com>
2022-10-14 12:34:34 -0700

    create a tablebase download script.

---
+++ syzygy-download/src/Lib.hs
@@ -0,0 +1,33 @@
+module Lib
+  ( main
+  )
+where
+
+import qualified Data.ByteString.Lazy as BSL
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import Data.Text.Encoding (decodeUtf8)
+import Network.HTTP.Client
+import Network.HTTP.Client.TLS (newTlsManager)
+import System.Environment
+import System.Exit
+
+fileListUri = "https://syzygy-tables.info/download.txt?source=lichess&max-pieces=6"
+commonPrefix = "https://tablebase.lichess.ovh/tables/"
+
+--- https://aria2.github.io/manual/en/html/aria2c.html#input-file
+genInputFileEntity targetBase uri = [uri, "  dir=" <> T.pack targetBase, "  out=" <> targetPath]
+  where
+    Just targetPath = T.stripPrefix commonPrefix uri
+
+main :: IO ()
+main =
+  getArgs >>= \case
+    ["prepare-list", targetBase] -> do
+      mgr <- newTlsManager
+      req <- parseRequest fileListUri
+      resp <- httpLbs (setRequestCheckStatus req) mgr
+      let uris = T.words $ decodeUtf8 $ BSL.toStrict $ responseBody resp
+      mapM_ T.putStrLn $ concatMap (genInputFileEntity targetBase) uris
+      pure ()
+    _ -> die "<prog> prepare-list <target base dir>"
