82d345471d1249fb0e511b0d7fe48abc8363cbaa
Javran Cheng <Javran.C@gmail.com>
2022-10-14 23:17:01 -0700

    clean things up a bit.

--- syzygy-download/src/Lib.hs
+++ syzygy-download/src/Lib.hs
@@ -1,22 +1,30 @@
-module Lib
-  ( main
-  )
-where
+module Lib (
+  main,
+) where
 
 import qualified Data.ByteString.Lazy as BSL
+import Data.String (IsString)
 import qualified Data.Text as T
-import qualified Data.Text.IO as T
 import Data.Text.Encoding (decodeUtf8)
+import qualified Data.Text.IO as T
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS (newTlsManager)
 import System.Environment
 import System.Exit
 
+fileListUri :: IsString s => s
 fileListUri = "https://syzygy-tables.info/download.txt?source=lichess&max-pieces=6"
+
+commonPrefix :: IsString s => s
 commonPrefix = "https://tablebase.lichess.ovh/tables/"
 
 --- https://aria2.github.io/manual/en/html/aria2c.html#input-file
-genInputFileEntity targetBase uri = [uri, "  dir=" <> T.pack targetBase, "  out=" <> targetPath]
+genInputFileEntity :: FilePath -> T.Text -> [T.Text]
+genInputFileEntity targetBase uri =
+  [ uri
+  , "  dir=" <> T.pack targetBase
+  , "  out=" <> targetPath
+  ]
   where
     Just targetPath = T.stripPrefix commonPrefix uri
 
