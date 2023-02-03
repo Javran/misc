d2b3549869e863a60a913e00b9d0ab41e5f3e4b0
Javran Cheng <Javran.C@gmail.com>
2023-02-02 21:27:17 -0800

    impl simulation.

--- paperclips/src/Lib.hs
+++ paperclips/src/Lib.hs
@@ -2,5 +2,105 @@ module Lib (
   main,
 ) where
 
+import Control.Monad.RWS.CPS
+import Control.Monad.Reader
+import qualified Data.Text as T
+import Data.Tuple
+import qualified Data.Vector.Unboxed as VU
+import qualified Data.Vector.Unboxed.Mutable as VUM
+import Data.Word
+import System.Random.MWC
+import Text.Printf
+
+data Move = A | B
+
+other :: Move -> Move
+other = \case
+  A -> B
+  B -> A
+
+{-
+  note: pickMove always assume itself to be the "horizontal" one.
+ -}
+data Strategy = Strategy
+  { sName :: T.Text
+  , sPickMove :: Payoff -> (Move, Move) -> ReaderT GenIO IO Move
+  }
+
+type Score = Int
+
+type Payoff = (Score, Score, Score, Score) -- aa, ab, ba, bb
+
+biggestPayoff :: Payoff -> (Move, Move)
+biggestPayoff (aa, ab, ba, bb)
+  | aa == best = (A, A)
+  | ab == best = (A, B)
+  | ba == best = (B, A)
+  | otherwise = (B, B)
+  where
+    best = max aa (max ab (max ba bb))
+
+strats :: [Strategy]
+strats =
+  [ Strategy "RANDOM" \_ _ -> do
+      g <- ask
+      b <- uniformM g
+      pure if b then A else B
+  , Strategy "A100" \_ _ -> pure A
+  , Strategy "B100" \_ _ -> pure B
+  , Strategy "GREEDY" \p _ -> pure $ fst $ biggestPayoff p
+  , Strategy "GENEROUS" \p _ -> pure $ snd $ biggestPayoff p
+  , Strategy "MINIMAX" \p _ -> pure $ other $ snd $ biggestPayoff p
+  , Strategy "TIT FOR TAT" \_ (_, oppo) -> pure oppo
+  , Strategy "BEAT LAST" \(aa, ab, ba, bb) (_, oppo) -> pure case oppo of
+      A -> if aa > ba then A else B
+      B -> if ab > bb then A else B
+  ]
+
+{-
+  The original code have this messy way of generating pairings that
+  it's probably best that we retain this behavior - it does matter as
+  some strategies rely on knowing previous moves.
+ -}
+pairings :: [((Int, Strategy), (Int, Strategy))]
+pairings =
+  [(sHd, v) | v <- ss]
+  <> [(h, v) | h <- sTl, v <- ss']
+  where
+    ss = zip [0 ..] strats
+    ~(sHd : sTl) = ss
+    ss' = sTl <> [sHd]
+
+runTourney :: GenIO -> (Move, Move) -> Payoff -> (Int -> Score -> IO ()) -> IO ()
+runTourney g initPrev p@(aa, ab, ba, bb) applyPayoff = do
+  let pairings' = concatMap (replicate 10) pairings
+      evalRound prev ((indH, sH), (indV, sV)) = do
+        hm <- sPickMove sH p prev
+        vm <- sPickMove sV p (swap prev)
+        let (payH, payV) = case (hm, vm) of
+              (A, A) -> (aa, aa)
+              (A, B) -> (ab, ba)
+              (B, A) -> (ba, ab)
+              (B, B) -> (bb, bb)
+        lift do
+          applyPayoff indH payH
+          applyPayoff indV payV
+        pure (hm, vm)
+  _ <- runReaderT (foldM evalRound initPrev pairings') g
+  pure ()
+
+repeatTourney :: GenIO -> (Move, Move) -> Payoff -> Int -> IO ()
+repeatTourney g initPrev p n = do
+  v <- VUM.replicate (length strats) (0 :: Score)
+  replicateM_ n (runTourney g initPrev p (\i incr -> VUM.modify v (+ incr) i))
+  forM_ (zip [0..] strats) \(i, s) -> do
+    let fI = fromIntegral @Int @Double
+    sc <- VUM.read v i
+    let ave = fI sc / fI n
+    printf "%20s\t%f\n" (sName s) ave
+    pure ()
+
 main :: IO ()
-main = pure ()
+main = do
+  g <- create
+  repeatTourney g (A, A) (10,10,10,7) 1000
