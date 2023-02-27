f27d5318465c1154bdc1f8e1f612a203d5830a1c
Javran Cheng <Javran.C@gmail.com>
2023-02-26 17:01:12 -0800

    verify current impl with quickcheck

--- sorting-network/src/Lib.hs
+++ sorting-network/src/Lib.hs
@@ -3,13 +3,39 @@ module Lib (
 ) where
 
 import Control.Monad
+import Control.Monad.ST
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as VM
+import Test.QuickCheck
 
 main :: IO ()
-main = mapM_ print (gen 8)
+main =
+  quickCheck $ withMaxSuccess 10000 do
+    l <- chooseInt (3, 30)
+    -- 0-1 principle
+    xs <- replicateM l (chooseEnum (False, True))
+    let ys = mySort xs
+    pure $ and $ zipWith (<=) ys (tail ys)
+
+mySort :: Ord a => [a] -> [a]
+mySort xs = runST do
+  v <- V.unsafeThaw (V.fromList xs)
+  let n = VM.length v
+  forM_ (gen n) \(i, j) -> do
+    vI <- VM.unsafeRead v i
+    vJ <- VM.unsafeRead v j
+    when (vI > vJ) do
+      VM.swap v i j
+
+  mapM (VM.unsafeRead v) [0 .. n - 1]
 
 {-
   Ref: https://en.wikipedia.org/wiki/Batcher_odd%E2%80%93even_mergesort
+
+  Note: index goes out of bound when n is not a power of 2.
+  unclear if we can simply just ignore those values?
  -}
+gen :: Int -> [] (Int, Int)
 gen n = do
   p <- takeWhile (< n) $ iterate (* 2) 1
   k <- takeWhile (>= 1) $ iterate (\v -> quot v 2) p
@@ -24,4 +50,5 @@ gen n = do
     floor (fromIntegral n / fromIntegral (2^k)) === shiftR n k
    -}
   guard $ floor @_ @Integer (fI (i + j) / fI (p * 2)) == floor (fI (i + j + k) / fI (p * 2))
+  guard $ i + j + k < n
   pure (i + j, i + j + k)
