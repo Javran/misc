2dc949a78d2cd9473c9c196ff3d1040e4f4eb2f8
Javran Cheng <Javran.C@gmail.com>
2023-02-27 23:02:46 -0800

    use qq to do most of the stuff.

--- sorting-network/src/TH.hs
+++ sorting-network/src/TH.hs
@@ -2,10 +2,13 @@
 
 module TH where
 
-import Control.Monad.State
+import Control.Monad
+import Control.Monad.IO.Class
 import qualified Data.Vector as V
 import qualified Data.Vector.Mutable as VM
 import Language.Haskell.TH
+
+type PartQ = Exp -> Q Exp
 
 gMkSorter :: (Int -> [(Int, Int)]) -> Int -> ([Pat] -> Pat) -> ([Exp] -> Exp) -> Q Exp
 gMkSorter mkPairs n mkP mkE = do
@@ -18,13 +21,13 @@ gMkSorter mkPairs n mkP mkE = do
 
   ns0 <- replicateM n $ newName "v"
   let -- let sw = ... in <???>
-      step0 :: Exp -> Exp
-      step0 bd = LetE [ValD (VarP swapper) (NormalB swapperVal) []] bd
-  (mkBody :: Exp -> Q Exp, ns :: [Name]) <- do
+      step0 :: PartQ
+      step0 bd = [|let $(varP swapper) = $(pure swapperVal) in $(pure bd)|]
+  (mkBody :: PartQ, ns :: [Name]) <- do
     nv <- liftIO $ V.unsafeThaw (V.fromList ns0)
     e <-
       foldM
-        ( \(mk :: Exp -> Q Exp) (i, j) -> do
+        ( \(mk :: PartQ) (i, j) -> do
             iOld <- liftIO $ VM.unsafeRead nv i
             jOld <- liftIO $ VM.unsafeRead nv j
             iNew <- newName "v"
@@ -41,13 +44,16 @@ gMkSorter mkPairs n mkP mkE = do
                     (\ $(varP iNew) $(varP jNew) -> $(pure hole))
                   |]
         )
-        (pure . step0)
+        step0
         pairs
     nvFin <- liftIO $ V.unsafeFreeze nv
     pure (e, V.toList nvFin)
 
-  r <- mkBody $ mkE $ VarE <$> ns
-  pure $ LamE [VarP cmp, mkP $ fmap VarP ns0] r
+  [|
+    \ $(varP cmp)
+      $(pure $ mkP $ VarP <$> ns0) ->
+        $(mkBody $ mkE $ VarE <$> ns)
+    |]
 
 mkSorterList, mkSorterTup :: (Int -> [(Int, Int)]) -> Int -> ExpQ
 mkSorterList mkPairs n = gMkSorter mkPairs n ListP ListE
