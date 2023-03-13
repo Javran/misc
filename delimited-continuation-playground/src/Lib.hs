a323f9e1d23ce2b7a17051fd9689daf79ac35aaa
Javran Cheng <Javran.C@gmail.com>
2023-03-12 21:47:46 -0700

    the initial stuff.

--- delimited-continuation-playground/src/Lib.hs
+++ delimited-continuation-playground/src/Lib.hs
@@ -2,5 +2,21 @@ module Lib (
   main,
 ) where
 
+import Control.Monad
+import Control.Monad.Trans.Cont
+
+t1 :: Cont w Integer
+t1 =
+  liftM2
+    (-)
+    ( reset
+        ( liftM2
+            (+)
+            (pure 3)
+            (shift (\k -> liftM2 (*) (pure 5) (pure 2)))
+        )
+    )
+    (pure 1)
+
 main :: IO ()
-main = pure ()
+main = print $ evalCont t1
