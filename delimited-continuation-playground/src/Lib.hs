214869e2281a23ea5af96e8dc13e4b0743ee2f31
Javran Cheng <Javran.C@gmail.com>
2023-03-12 22:05:47 -0700

    deviate a bit from the example
    just to show an example where continuation is used.

--- delimited-continuation-playground/src/Lib.hs
+++ delimited-continuation-playground/src/Lib.hs
@@ -15,5 +15,31 @@ t1 =
       )
     <*> pure 1
 
+t1Extra :: Cont w Integer
+t1Extra =
+  (-)
+    <$> reset
+      ( fmap
+          (3 +)
+          ( shift
+              ( \k -> do
+                  {-
+                    here k is `3 + {}`.
+                    in addition to computing 5*2 inside,
+                    we "insert another layer" between
+                    reseting point and final result of this,
+                    or `8 + {}`
+                    resulting in
+                    `8 + (3 + {5*2})` being final result for
+                    this part of the computation.
+                   -}
+                  pure $ 8 + k (5 * 2)
+              )
+          )
+      )
+    <*> pure 1
+
 main :: IO ()
-main = print $ evalCont t1
+main = do
+  print $ evalCont t1
+  print $ evalCont t1Extra
